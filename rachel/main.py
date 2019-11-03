8ecd22e8e0f1559351726d47910bdbbe4b1b4c8f
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-03 20:18:13 -0300

    Saved rachel/main.py @2019-11-03 23:18

--- rachel/main.py
+++ rachel/main.py
@@ -10,7 +10,7 @@ def Historia():
                        tit="talita",
                        style=dict(left=100, top=90,  Width=60, height=50))
 	talita.entra(quartotalita)                       
-	textotalita = Texto (quartotalita, "Olá. Hoje vai ser um dia longo e eu preciso estar preparada para encarar muitos desafios. O primeiro deles é estar protegida. Hoje sairei de Costa Barros protegida e contarei com uma invenção femina para isso. Me ajude a encontrar.")
+	textotalita = Texto (quartotalita, "Olá. Hoje vai ser um dia longo e eu preciso estar preparada para encarar muitos desafios. Hoje sairei de Costa Barros protegida e contarei com uma invenção femina para isso.")
 	talita.vai = textotalita.vai
 	quartotalita.vai ()
 Historia()
