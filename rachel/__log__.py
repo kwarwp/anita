be7c0c5f11e00aa4894c0cf711300e5d36853824
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-03 19:37:18 -0300

    Created rachel/__log__.py @2019-11-03 22:37

---
+++ rachel/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Sun Nov 03 2019 19:37:16.113 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 3
+  linkdamoana ="https://i.imgur.com/Z56lQfP.jpg'
+                                                ^
+SyntaxError: EOL while scanning string literal
+'''},
