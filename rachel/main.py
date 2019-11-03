1bc1a8887d80b00fb5f300f6d35b15a66cabd622
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-03 19:30:31 -0300

    Saved rachel/main.py @2019-11-03 22:30

--- rachel/main.py
+++ rachel/main.py
@@ -1,6 +1,6 @@
 # anita.rachel.main.py
 from _spy.vitollino.main import Cena, Elemento, Texto
-linkdamoana ="https://vignette.wikia.nocookie.net/characterprofile/images/6/64/Hertmoana.png/revision/latest?cb=20180112235349"
+linkdamoana ="https://i.imgur.com/DXk4lIb.jpg"
 def Historia():
 	cenaPraiamoana = Cena (img = "https://img.elo7.com.br/product/original/16965E0/painel-praia-g-frete-gratis-infanitl.jpg")
 	moana = Elemento (img = linkdamoana, 
