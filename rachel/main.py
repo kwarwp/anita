f0ebc49633b3afcea0e1a1cfdf2b04b0071be538
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-03 19:56:32 -0300

    Saved rachel/main.py @2019-11-03 22:56

--- rachel/main.py
+++ rachel/main.py
@@ -2,7 +2,7 @@
 from _spy.vitollino.main import Cena, Elemento, Texto
 linkdamoana ="https://i.imgur.com/N5HXcxK.png"
 def Historia():
-	cenaPraiamoana = Cena (img = "https://img.elo7.com.br/product/original/16965E0/painel-praia-g-frete-gratis-infanitl.jpg")
+	cenaPraiamoana = Cena (img = "https://i.imgur.com/KmfTQCq.jpg")
 	moana = Elemento (img = linkdamoana, 
                        tit="moana",
                        style=dict(left=10, top=60,  Width=60, height=50))
