578c6354fe5a4831780972161fbf4b9a00d8dd96
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-03 20:09:22 -0300

    Saved rachel/main.py @2019-11-03 23:09

--- rachel/main.py
+++ rachel/main.py
@@ -1,11 +1,14 @@
 # anita.rachel.main.py
-from _spy.vitollino.main import Cena, Elemento, Texto
+from _spy.vitollino.main import Cena, Elemento, Texto, STYLE
+STYLE["width"] = 1200
+STYLE["heigth"] = "1000px"
 linkdamoana ="https://i.imgur.com/N5HXcxK.png"
 def Historia():
 	cenaPraiamoana = Cena (img = "https://i.imgur.com/Sj9T2Y8.gif")
+    
 	moana = Elemento (img = linkdamoana, 
                        tit="moana",
-                       style=dict(left=10, top=60,  Width=100, height=50))
+                       style=dict(left=10, top=60,  Width=60, height=50))
 	moana.entra(cenaPraiamoana)                       
 	txtmoana = Texto (cenaPraiamoana, "Hello")
 	moana.vai = txtmoana.vai
