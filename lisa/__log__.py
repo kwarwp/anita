85f7897677a4498af18f0044374238baa22c7da2
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-04 09:50:33 -0300

    Saved lisa/__log__.py @2019-11-04 12:50

--- lisa/__log__.py
+++ lisa/__log__.py
@@ -20,3 +20,10 @@ SyntaxError: invalid syntax
                                                             ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Mon Nov 04 2019 09:50:32.759 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 9
+  talita= Elemento (img= linkdatalita 
+                                                             ^
+SyntaxError: invalid syntax
+'''},
