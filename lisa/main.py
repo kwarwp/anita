23018ea290a41296da609225ad44559c9dd7f3b5
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-04 10:04:54 -0300

    Saved lisa/main.py @2019-11-04 13:04

--- lisa/main.py
+++ lisa/main.py
@@ -6,9 +6,9 @@ linkdatalita="https://www.pngix.com/pngfile/middle/341-3413543_menina-boneca-des
 linkdocolete="https://i.imgur.com/mbj8tzc.png"
 def Jogo ():
     quartodatalita= Cena (img= "https://image.freepik.com/vetores-gratis/modelo-de-plano-de-fundo-interior-quarto-dos-desenhos-animados-aconchegante-casa-moderna-sala-na-luz-da-manha_33099-171.jpg")
-    talita= Elemento (img= linkdatalita,
+    talita = Elemento (img = linkdatalita,
                      tit="talita",
-                     style=dict (180,top=50,width=,hight) 
+                     style=dict (left 180,top=50,width=60,height 50))
     talita.entra(quartodatalita)
     textotalita=(quartotalita," Para mim sai do quarto,preciso que vc ache para mim o objeto que stephanie kudlek criou "
     textocolete=texto (quartotalita, "parabéns,vc encontou o colete!tenho um presente pra vc")
