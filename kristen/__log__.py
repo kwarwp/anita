3a231ec556cfee78798d327390e54baf79705d01
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-04 09:51:30 -0300

    Saved kristen/__log__.py @2019-11-04 12:51

--- kristen/__log__.py
+++ kristen/__log__.py
@@ -6,3 +6,10 @@
   ^
 IndentationError: unexpected indent
 '''},
+{'date': 'Mon Nov 04 2019 09:51:29.258 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 9
+  talita=Elemento(img="linkdatalita")
+  ^
+IndentationError: unexpected indent
+'''},
