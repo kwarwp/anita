e75c84ca0229822720c3575b8da9548f0a904374
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-04 09:48:59 -0300

    Created lisa/__log__.py @2019-11-04 12:48

---
+++ lisa/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Mon Nov 04 2019 09:48:57.361 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 9
+  talita= Elemento (imag=linkdatalita 
+                                                             ^
+SyntaxError: invalid syntax
+'''},
