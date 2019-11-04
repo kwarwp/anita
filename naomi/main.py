251c3e6b297bf77925bcb4026738f000911fc482
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-04 10:38:24 -0300

    Saved naomi/main.py @2019-11-04 13:38

--- naomi/main.py
+++ naomi/main.py
@@ -5,14 +5,14 @@ STYLE["heigth"]= "200px"
 linkdatalita="https://i.imgur.com/2LZFFjU.png"
 linkcolete="https://i.imgur.com/lWiNq2H.png"
 def Jogo():
-	quartodatalita=Cena(img="https://i.imgur.com/DEC5m3T.jpg")
+	quartodatalita=Cena("img=https://i.imgur.com/m6fxFJf.jpg")
 	talita=Elemento(img="linkdatalita",
                      tit="talita",
                      style=dict(left=180, top=50, width=60, heith=50))
      
-	colete=Elemento(img="linkcolete",
+	colete = Elemento(img="linkcolete",
 	tit="colete",
-	style=dict(left=90, top=25, width=30, heith=25))
+	style=dict(left=80, top=10, width=40 heith=10))
    	talita.entra(quartodatalita)
    	colete.entra(quartodatalita)
    	textotalita=Texto(quartodatalita,"hey,encontre o objeto criado por stephanie kwolek e ganhe moedas") 
