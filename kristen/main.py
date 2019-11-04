8a4a25f93a188379d8caf232d2d63f8b756a3409
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-04 10:01:10 -0300

    Saved kristen/main.py @2019-11-04 13:01

--- kristen/main.py
+++ kristen/main.py
@@ -6,15 +6,15 @@ linkdatalita = "https://imagensemoldes.com.br/wp-content/uploads/2018/03/Bonecas
 linkcoletetalita =  "https://i.imgur.com/VAVNngX.png"
 def Jogo():
 	quartodatalita=Cena(img="https://i.pinimg.com/736x/d4/50/3e/d4503ef624a8c8212e992d6e93047c4f.jpg")
-	talita=Elemento(img=linkdatalita,
-				tit="talita"
+	talita = Elemento(img = linkdatalita,
+				tit="talita",
 				style= dict (left=180, top = 50, width= 60, height=50))
-    talita.entra(quartodatalita)
-    textotalita=Texto(quartotalita,"Olá! Me ajude a encontrar o objeto criado por Stephanie Kwolek e ganhe moedas")
-    textocolete=Texto(quartotalita,"Parabéns! Vista o colete")
-    talita.vai=textotalita.vai
-    colete.vai=textocolete.vai
-    quartotalita.vai()
+	talita.entra(quartodatalita)
+      textotalita=Texto(quartotalita,"Olá! Me ajude a encontrar o objeto criado por Stephanie Kwolek e ganhe moedas")
+	textocolete=Texto(quartotalita,"Parabéns! Vista o colete")
+	talita.vai=textotalita.vai
+	colete.vai=textocolete.vai
+	quartotalita.vai()
 
 Jogo()
          
