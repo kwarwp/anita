4325f1526b982c8534be64bd934f491d60e21772
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-04 10:40:00 -0300

    Saved naomi/__log__.py @2019-11-04 13:39

--- naomi/__log__.py
+++ naomi/__log__.py
@@ -334,3 +334,10 @@ SyntaxError: invalid syntax
                                                                                              ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Mon Nov 04 2019 10:39:59.372 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 13
+  colete = Elemento(img = "linkcolete",
+                                                                                                                ^
+SyntaxError: invalid syntax
+'''},
