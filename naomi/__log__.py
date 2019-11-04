bfb9dbd2c3a5443a5603d102443d11c552caa2cc
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-04 09:47:26 -0300

    Created naomi/__log__.py @2019-11-04 12:47

---
+++ naomi/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Mon Nov 04 2019 09:48:08.167 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 10
+  tit="talita",
+  ^
+IndentationError: unexpected indent
+'''},
