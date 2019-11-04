372d104d7200b26376b1cb52aaaeb92644b71f4f
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-04 10:21:06 -0300

    Saved kristen/__log__.py @2019-11-04 13:21

--- kristen/__log__.py
+++ kristen/__log__.py
@@ -235,3 +235,10 @@ IndentationError: unexpected indent
                                                                                                     ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Mon Nov 04 2019 10:21:05.551 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 13
+  colete = Elemento(img=linkcoletetalita,
+                                                                                                        ^
+SyntaxError: invalid syntax
+'''},
