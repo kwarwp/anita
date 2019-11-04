eae85df8c2138ba272c7d16108928bf2b38c8794
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-04 09:54:09 -0300

    Saved naomi/main.py @2019-11-04 12:54

--- naomi/main.py
+++ naomi/main.py
@@ -9,10 +9,10 @@ def jogo():
 	talita=elemento(img="linkdatalita",
                      tit="talita",
                      style=dict (left=180,top=50,width=60,hight=50)) 
-   talita.entra(quartodatalita)
-   textotalita=texto(quartotalita,"hey,encontre o objeto criado por stephanie kwolek e ganhe moedas!") 
-   textotalita=texto(quartotalita,"parabéns! vista o colete e passe para a proxima fase!")
-   talita.vai=textotalita.vai
-   colete.vai=textocolete.vai
-   quartotalita.vai()
+   	talita.entra(quartodatalita)
+   	textotalita=texto(quartotalita,"hey,encontre o objeto criado por stephanie kwolek e ganhe moedas") 
+   	textotalita=texto(quartotalita,"parabéns! vista o colete e passe para a proxima fase!")
+   	talita.vai=textotalita.vai
+   	colete.vai=textocolete.vai
+   	quartotalita.vai()
 jogo()
