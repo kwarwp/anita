8859ddf066ff8f040159d7330815d767a71c44b1
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-04 09:47:11 -0300

    Created lisa/main.py @2019-11-04 12:47

---
+++ lisa/main.py
@@ -0,0 +1,18 @@
+# anita.lisa.main.py
+from _spy.vitollino.main import Cena,Elemento,Texto,STYLE
+STYLE ["width"]=600
+STYLE ["hugyh"]= "200px"
+linkdatalita="https://www.pngix.com/pngfile/middle/341-3413543_menina-boneca-desenho-hd-png-download.png"
+linkdocolete="https://i.imgur.com/mbj8tzc.png"
+def Jogo ():
+    quartodatalita= Cena (img= "https://image.freepik.com/vetores-gratis/modelo-de-plano-de-fundo-interior-quarto-dos-desenhos-animados-aconchegante-casa-moderna-sala-na-luz-da-manha_33099-171.jpg")
+    talita= Elemento (imag=linkdatalita 
+                     tit="talita",
+                     style=dict (180,top=50,width=,hight) 
+    talita.entra(quartodatalita)
+    textotalita=(quartotalita," Para mim sai do quarto,preciso que vc ache para mim o objeto que stephanie kudlek criou "
+    textocolete=texto (quartotalita, "parabéns,vc encontou o colete!tenho um presente pra vc")
+    
+                    
+                   
+    
