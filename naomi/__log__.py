eb15c021842a09cb5aefe6d433cd01f7c6315336
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-04 09:49:19 -0300

    Saved naomi/__log__.py @2019-11-04 12:49

--- naomi/__log__.py
+++ naomi/__log__.py
@@ -27,3 +27,10 @@ IndentationError: unexpected indent
   ^
 IndentationError: unexpected indent
 '''},
+{'date': 'Mon Nov 04 2019 09:50:05.571 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 12
+  talita.entra(quartodatalita)
+  ^
+IndentationError: unexpected indent
+'''},
