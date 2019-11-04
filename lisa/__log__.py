36c014cd626a27a86b4d870d911f89a8de20c7a4
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-04 10:08:20 -0300

    Saved lisa/__log__.py @2019-11-04 13:08

--- lisa/__log__.py
+++ lisa/__log__.py
@@ -140,3 +140,21 @@ NameError: name 'jogo' is not defined
     textocolete=texto (quartodatalita, "parabéns,vc encontou o colete!tenho um presente pra vc")
 NameError: name 'texto' is not defined
 '''},
+{'date': 'Mon Nov 04 2019 10:08:19.433 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 18
+    Jogo()
+  module <module> line 16
+    colete.vai=textocolete.vai
+NameError: name 'colete' is not defined
+'''},
