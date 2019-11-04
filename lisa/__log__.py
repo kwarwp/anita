03ac729008b682507939bbf5d46342ecfea6401b
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-04 10:06:39 -0300

    Saved lisa/__log__.py @2019-11-04 13:06

--- lisa/__log__.py
+++ lisa/__log__.py
@@ -76,3 +76,10 @@ SyntaxError: invalid syntax
                                                                                                                                ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Mon Nov 04 2019 10:06:38.502 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 13
+  textotalita=(quartodatalita,"Para mim sai do quarto,preciso que vc ache para mim o objeto que stephanie kudlek criou"
+                                                                                                                             ^
+SyntaxError: invalid syntax
+'''},
