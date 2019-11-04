0943dbbf95d95135f12d3a208066054e64c7b59f
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-04 10:07:07 -0300

    Saved lisa/main.py @2019-11-04 13:07

--- lisa/main.py
+++ lisa/main.py
@@ -10,7 +10,7 @@ def Jogo ():
                      tit="talita",
                      style=dict (left=180,top=50,width=60,height=50))
     talita.entra(quartodatalita)
-    textotalita=(quartodatalita,"Para mim sai do quarto preciso que vc ache para mim o objeto que stephanie kudlek criou"
+    textotalita=(quartodatalita,"Para mim sai do quarto preciso que vc ache para mim o objeto que stephanie kudlek criou")
     textocolete=texto (quartodatalita, "parabéns,vc encontou o colete!tenho um presente pra vc")
     talita.vai=textotalita.vai
     colete.vai=textocolete.vai
