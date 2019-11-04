cef74e7ebe9e556464219b6a97d5c59b2d0ee2c5
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-04 09:47:06 -0300

    Saved naomi/main.py @2019-11-04 12:47

--- naomi/main.py
+++ naomi/main.py
@@ -1,1 +1,18 @@
 # anita.naomi.main.py
+from _spy.vitollino.main import Cena,Elemento,Texto,STYLE
+STYLE["width"]= 600
+STYLE["heigth"]= "200px"
+linkdatalita="https://i.imgur.com/2LZFFjU.png"
+linkcolete="https://i.imgur.com/lWiNq2H.png"
+def jogo():
+	quartodatalita=cena(img="https://i.imgur.com/DEC5m3T.jpg")
+	talita=elemento(img="linkdatalita")
+                     tit="talita",
+                     style=dict (left=180,top=50,width=60,hight=50)) 
+      talita.entra(quartodatalita)
+      textotalita=texto(quartotalita,"hey,encontre o objeto criado por stephanie kwolek e ganhe moedas!") 
+      textotalita=texto(quartotalita,"parabéns! vista o colete e passe para a proxima fase!")
+      talita.vai=textotalita.vai
+      colete.vai=textocolete.vai
+      quartotalita.vai()
+jogo()
