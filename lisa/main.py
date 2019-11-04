078cdf06668b82c8dcf9b7958ea27c05ace24427
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-04 10:46:03 -0300

    Saved lisa/main.py @2019-11-04 13:46

--- lisa/main.py
+++ lisa/main.py
@@ -2,11 +2,11 @@
 from _spy.vitollino.main import Cena,Elemento,Texto,STYLE
 STYLE ["width"]=600
 STYLE ["height"]= "200px"
-linkdatalita="https://i.imgur.com/8gMTfMm.png"
+linkdatalita="https://imagensemoldes.com.br/wp-content/uploads/2018/03/Bonecas-LOL-Serie-2-Cosplay-Club-Midnight-PNG-220x300.png"
 linkdocolete="https://i.imgur.com/mbj8tzc.png"
 def Jogo ():
     quartodatalita= Cena (img= "https://image.freepik.com/vetores-gratis/modelo-de-plano-de-fundo-interior-quarto-dos-desenhos-animados-aconchegante-casa-moderna-sala-na-luz-da-manha_33099-171.jpg")
-    talita = Elemento (img ="linkdatalita",
+    talita = Elemento (img =linkdatalita,
 				tit="talita",
 				style=dict (left=300,top=50,width=60,height=50))
                      
