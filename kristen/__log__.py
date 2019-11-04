7801c603323edca2c63a173b9bac5f26b4002ccb
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-04 10:26:41 -0300

    Saved kristen/__log__.py @2019-11-04 13:26

--- kristen/__log__.py
+++ kristen/__log__.py
@@ -263,3 +263,10 @@ SyntaxError: invalid syntax
                                                                          ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Mon Nov 04 2019 10:26:40.993 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 13
+  colete = Elemento (img = linkcoletetalita,
+                                                                         ^
+SyntaxError: invalid syntax
+'''},
