bbe214b08ce105c4d8b164d29c8204300f18534c
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-03 21:21:46 -0300

    Saved rachel/__log__.py @2019-11-04 00:21

--- rachel/__log__.py
+++ rachel/__log__.py
@@ -397,3 +397,21 @@ TypeError: 'Cena' object is not iterable
     sala,esquerda = quartotalita
 TypeError: 'Cena' object is not iterable
 '''},
+{'date': 'Sun Nov 03 2019 21:21:46.25 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 28
+    Historia()
+  module <module> line 27
+    sala,esquerda = quartotalita
+TypeError: 'Cena' object is not iterable
+'''},
