39fbecbb4aae67ca371e47330630773e739e8fe4
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-11 11:05:46 -0300

    Created soraya/main.py @2019-11-11 14:05

---
+++ soraya/main.py
@@ -0,0 +1,20 @@
+# anita.soraya.main.py
+from _spy.vitollino.main  import Cena, Elemento, Texto,STYLE
+STYLE["width"]= 600
+STYLE["height"]= "200px"
+linkcolete="https://www.camuflagemairsoft.com.br/media/catalog/product/cache/1/image/800x/9df78eab33525d08d6e5fb8d27136e95/4/7/475168341_VERDE.png.png"
+linkdatalita="https://i.pinimg.com/originals/f8/b7/8f/f8b78f6a2e48d6478d606b7ea304b169.png"
+linkquartodatalita="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcREI1tc3ljKAM-C5hZTeauUtZ5sB2_omrRWaqszuHqo9vGDPNPbKg&s"
+def Jogo ():
+	quartodatalita=Cena(img="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcREI1tc3ljKAM-C5hZTeauUtZ5sB2_omrRWaqszuHqo9vGDPNPbKg&s")
+	Talita= Elemento (img= linkdatalita
+				tit="talita",
+				style=dict(left=180, top=50, width=60, heith=50))
+
+	colete= Elemento(img=linkcolete,
+	tit="colete"
+	style=dict(left=90, top=25, width=30,helih=25))
+    
+		talita.entra(quartodatalita)
+		Conlete.enrta(quartodatalita) 
+		textotalita=texto(quartodata, " 
