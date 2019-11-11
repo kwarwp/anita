b80510a1195a4eb93d0b73d5e9c45ee8cef5cc64
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-11 11:04:24 -0300

    Saved amanda/__log__.py @2019-11-11 14:04

--- amanda/__log__.py
+++ amanda/__log__.py
@@ -27,3 +27,10 @@ SyntaxError: trailing comma not allowed without surrounding parentheses
                                  ^
 SyntaxError: trailing comma not allowed without surrounding parentheses
 '''},
+{'date': 'Mon Nov 11 2019 11:04:32.644 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 9
+  quartodatalita=Cena(img="linkquartotalita")
+  ^
+IndentationError: expected an indented block
+'''},
