eb4b8010b6fefcc02bc0c24533dbf57b0775e437
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-11 10:47:28 -0300

    Saved sarah/main.py @2019-11-11 13:47

--- sarah/main.py
+++ sarah/main.py
@@ -6,9 +6,12 @@ linkdatalita="https://img.elo7.com.br/product/original/222F529/desenho-boneca-lo
 linkquartodatalita="https://cdna.artstation.com/p/assets/images/images/008/176/322/large/ian-barreto-anymalu-quarto-ianbarreto.jpg?1510953832""
 linkcoletedatalita="https://i.imgur.com/IZkZR1d.jpg"
 def jogo():
-quartodatalita=Cena(tmg="https://img.elo7.com.br/product/original/222F529/desenho-boneca-lol-para-festa-de-crianca-menina.jpg"
-coletedatalita=Elemento (tmg="https://i.imgur.com/IZkZR1d.jpg"
-talita=Elemento(img="https://img.elo7.com.br/product/original/222F529/desenho-boneca-lol-para-festa-de-crianca-menina.jpg")
+quartodatalita=Cena(img="linkquartotalita")
+                    tit="quarto"
+                    style=dict(left=80, top=25, width=30, heith=30))
+coletedatalita=Elemento (img="linkcoletetalita")
+
+talita=Elemento(img="linkdatalita")
 			tit="talita
  			style=dict(left=180.top=50,width=60,heih=50))
 colete=Elemento(img="https://i.imgur.com/IZkZR1d.jpg")
