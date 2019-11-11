63f9b078eda238b225fb0788d90d2015ec2abc56
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-11 10:34:48 -0300

    Created sarah/main.py @2019-11-11 13:34

---
+++ sarah/main.py
@@ -0,0 +1,14 @@
+# anita.sarah.main.py
+from _spy.vitollimo.main import Cena, Elemento,Texto,STYLE
+STYLE[wilth]=600
+STYLE[heigh]="200px"
+linkdatalita="https://img.elo7.com.br/product/original/222F529/desenho-boneca-lol-para-festa-de-crianca-menina.jpg"
+linkquartodatalita="https://cdna.artstation.com/p/assets/images/images/008/176/322/large/ian-barreto-anymalu-quarto-ianbarreto.jpg?1510953832""
+linkcoletedatalita="https://i.imgur.com/IZkZR1d.jpg"
+def jogo():
+quartodatalita=Cena(tmg="https://img.elo7.com.br/product/original/222F529/desenho-boneca-lol-para-festa-de-crianca-menina.jpg"
+coletedatalita=Elemento (tmg="https://i.imgur.com/IZkZR1d.jpg"
+talita=Elemento(img="https://img.elo7.com.br/product/original/222F529/desenho-boneca-lol-para-festa-de-crianca-menina.jpg")
+			tit="talita
+ 			style=dict(left=180.top=50,width=60,heih=50))
+colete=Elemento(img="https://i.imgur.com/IZkZR1d.jpg")
