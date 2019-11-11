ef2ea4434fa03ce8801c7c27f6056697fedff9cc
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-11 11:05:49 -0300

    Saved meredith/main.py @2019-11-11 14:05

--- meredith/main.py
+++ meredith/main.py
@@ -1,1 +1,15 @@
-# anita.meredith.main.py
+
+from _spy.vitollino.main import Cena,Elemento,Texto,STYLE
+STYLE["width"]= 600
+STYLE["heigh"]="200px"
+linkdaTalita="https://www.google.com/search?biw=1536&bih=754&tbm=isch&sa=1&ei=sVTJXZ-HF7S_5OUPybSyqA0&q=menina+desenho+desendentes+jpg&oq=menina+desenho+desendentes+jpg&gs_l=img.3...313613.315755..317248...0.0..0.551.1408.0j1j0j2j0j1......0....1..gws-wiz-img.KTaxQa_aeII&ved=0ahUKEwjfo76EleLlAhW0H7kGHUmaDNUQ4dUDCAc&uact=5#imgdii=S1LzO66A9prhcM:&imgrc=9H6EQNxskC89fM:"
+linKquartodaTalita="https://st3.depositphotos.com/8065304/15935/v/1600/depositphotos_159354828-stock-illustration-the-interior-of-the-bedroom.jpg"
+linkcoletedaTalita="https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwj3ssW-neLlAhUfGLkGHd2jCyQQjRx6BAgBEAQ&url=https%3A%2F%2Ffardasdelite.com%2Fpt%2Fcolete-a-prova-de-balas&psig=AOvVaw2yF6ItL3CQjMtTAPoFLUY0&ust=1573563884838448https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=08&ved=2ahUKEwj3ssW-neLlAhUfGLkGHd2jCyQQjRx6BAgBEAQ&url=https%3A%2F%2Ffardasdelite.com%2Fpt%2Fcolete-a-prova-de-balas&psig=AOvVaw2yF6ItL3CQjMtTAPoFLUY0&ust=1573563884838448"
+def jogo():
+quartodaTalita=Cena(img="linkquartoTalita")
+                    tit("quartodaTalita"
+                    style=dict(left=180, top=50, witdh=60, heith=50))
+coletedaTalita=Elemento(img="linkcoletedaTalita")
+                     tit("coletedaTalita"
+                     style=dict(left=170, top=40, witdh=50, heith=40))
+                              
