a33f20a1eb96e8ddb689101f4e816fc05ffd4a78
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-11 11:05:47 -0300

    Created soraya/__log__.py @2019-11-11 14:05

---
+++ soraya/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Mon Nov 11 2019 04:05:43.17 GMt-1000 (Horário Padrão do Havaí e Ilhas Aleutas) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 10
+  Talita= Elemento (img= linkdatalita
+                                           ^
+SyntaxError: invalid syntax
+'''},
