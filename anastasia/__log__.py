10aa9f46b99b33653a686b88866d5cd78c5c1849
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-17 15:55:20 -0300

    Saved anastasia/__log__.py @2019-11-17 18:55

--- anastasia/__log__.py
+++ anastasia/__log__.py
@@ -83,3 +83,10 @@ IndentationError: unexpected indent
   ^
 IndentationError: unexpected indent
 '''},
+{'date': 'Sun Nov 17 2019 15:55:19.321 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 24
+  livro.entra(quartotalita)
+  ^
+IndentationError: unexpected indent
+'''},
