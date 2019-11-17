b073bc8830149d59e2d775d373790a2df49de8b6
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-17 15:17:42 -0300

    Saved anastasia/main.py @2019-11-17 18:17

--- anastasia/main.py
+++ anastasia/main.py
@@ -17,9 +17,9 @@ def Historia():
 	livro = Elemento (img = linkdolivro,
 				tit="livro",
 				style=dict(left=20, top=10, width=13, height=20))
-      talita.entra(quartotalita)
+      	talita.entra(quartotalita)
       	colete.entra(quartotalita)
-       livro.entra(quartotalita)
+	    livro.entra(quartotalita)
      	textotalita = Texto (quartotalita, "Oi sou a Talita, venha comigo nessa aventura! Procure objetos que foram feitos por mulheres".
     	textocolete = Texto (quartotalita, "Stephanie Kwolek criou o colete à prova de balas Kevlar, que todos os anos salva a vida de milhares de policiais")
         textolivro = Texto (quartotalita,"O livro possui aventuras mágicas, que te ajudaram em alguns desafios".
