d207ed5076ebd7a1eeb82b0780afc5f8f8e72744
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-17 17:04:34 -0300

    Saved amanda/__log__.py @2019-11-17 20:04

--- amanda/__log__.py
+++ amanda/__log__.py
@@ -90,3 +90,10 @@ IndentationError: expected an indented block
                                                                                                          ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Sun Nov 17 2019 17:04:32.804 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 10
+  quartodatalita = Cena (img=linkquartodatalita,
+                                                                                                                             ^
+SyntaxError: invalid syntax
+'''},
