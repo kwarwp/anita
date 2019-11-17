7f8a35c435b78481feb46708fbd59a6bcbc78ea7
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-17 16:56:19 -0300

    Saved amanda/__log__.py @2019-11-17 19:56

--- amanda/__log__.py
+++ amanda/__log__.py
@@ -76,3 +76,10 @@ IndentationError: expected an indented block
   ^
 IndentationError: expected an indented block
 '''},
+{'date': 'Sun Nov 17 2019 16:56:18.295 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 10
+  quartodatalita = Cena (img="linkquartodatalita")
+  ^
+IndentationError: expected an indented block
+'''},
