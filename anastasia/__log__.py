ee2281ed25a6f72a3bd6f94711d62f0d804c2010
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-17 15:18:58 -0300

    Saved anastasia/__log__.py @2019-11-17 18:18

--- anastasia/__log__.py
+++ anastasia/__log__.py
@@ -34,3 +34,10 @@ IndentationError: unexpected indent
               ^
 SyntaxError: keyword can't be an expression
 '''},
+{'date': 'Sun Nov 17 2019 15:18:56.371 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 24
+  textodocolete = Texto (quartotalita, "Stephanie Kwolek criou o colete à prova de balas Kevlar, que todos os anos salva a vida de milhares de policiais")
+                ^
+SyntaxError: keyword can't be an expression
+'''},
