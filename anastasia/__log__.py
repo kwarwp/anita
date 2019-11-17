97e678ead5eed11be5763b9e7a815b0a5d2e1853
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-17 15:16:02 -0300

    Saved anastasia/__log__.py @2019-11-17 18:16

--- anastasia/__log__.py
+++ anastasia/__log__.py
@@ -6,3 +6,10 @@
                                                             ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Sun Nov 17 2019 15:16:01.119 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 20
+  talita.entra(quartotalita)
+  ^
+IndentationError: unexpected indent
+'''},
