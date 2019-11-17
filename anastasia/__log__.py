8cce323d76d194f3183b295cc25528a806cd8fb4
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-17 14:51:59 -0300

    Created anastasia/__log__.py @2019-11-17 17:51

---
+++ anastasia/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Sun Nov 17 2019 14:51:56.236 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 17
+  livro = Elemento (img = linkdolivro,
+                                                            ^
+SyntaxError: invalid syntax
+'''},
