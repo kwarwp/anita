e3960179b93ff213d14c591a311741b381eb6a15
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-18 07:19:54 -0300

    Saved heather/main.py @2019-11-18 10:19

--- heather/main.py
+++ heather/main.py
@@ -6,9 +6,9 @@ from _spy.vitollino.main import Cena, Elemento, Texto, STYLE
 def Historia():
 	introd = Cena (img = "https://i.imgur.com/Py9oguB.jpg")
     
-	botao = Elemento (img = "https://i.imgur.com/GGVktQI.png",
+	botao = Elemento (img = "https://i.imgur.com/hDAafpT.png",
 	tit="Jogar",
-	style=dict(left=50, top=50,  Width=50, height=50))
+	style=dict(left=250, top=300,  Width=300, height=300))
      
 	botao.entra(introd)
 	introd.vai()
