1a8702a9d2507d7ddf648436398379453eaf2426
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-18 07:55:46 -0300

    Saved heather/main.py @2019-11-18 10:55

--- heather/main.py
+++ heather/main.py
@@ -7,7 +7,7 @@ def Historia():
 	quartotalita = Cena (img = "https://lh5.googleusercontent.com/-fs1hatHWU9s/UUpy0DJqlXI/AAAAAAAAbs4/Vy1LL28sPeY/s400/tumblr_lt4n2aSjsX1qmvaoo.gif")
 	botao = Elemento (img = "https://i.imgur.com/hDAafpT.png",
 	tit="Jogar",
-	style=dict(left=250, top=400, width=120, heigth=120))
+	style=dict(left=240, top=400, width=120, heigth=120))
      
 	botao.entra(introd)
 	introd.vai()
