945e4ac2f96615e5cabfd91aa52c94d667f95ebd
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-18 07:45:37 -0300

    Saved heather/main.py @2019-11-18 10:45

--- heather/main.py
+++ heather/main.py
@@ -1,17 +1,17 @@
 # anita.heather.main.py
 from _spy.vitollino.main import Cena, Elemento, Texto, STYLE
-
-
-
+STYLE["width"] = 600
+STYLE["heigth"] = "200px"
 def Historia():
 	introd = Cena (img = "https://i.imgur.com/Py9oguB.jpg")
-	quartotalita = Cena (img = "https://lh5.googleusercontent.com/-fs1hatHWU9s/UUpy0DJqlXI/AAAAAAAAbs4/Vy1LL28sPeY/s400/tumblr_lt4
+	quartotalita = Cena (img = "https://lh5.googleusercontent.com/-fs1hatHWU9s/UUpy0DJqlXI/AAAAAAAAbs4/Vy1LL28sPeY/s400/tumblr_lt4n2aSjsX1qmvaoo.gif")
 	botao = Elemento (img = "https://i.imgur.com/hDAafpT.png",
 	tit="Jogar",
-	style=dict(x=0, y=900, left=200, top=280, width=200, height=200))
+	style=dict(left=200, top=250, width=120, height=80))
      
 	botao.entra(introd)
 	introd.vai()
 	botao.vai=quartotalita.vai
 	
+Historia()
 Historia()
