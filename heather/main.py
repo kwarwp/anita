daa44cdc63a3709e8bd42e04f78b724556d973e0
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-18 07:18:11 -0300

    Saved heather/main.py @2019-11-18 10:18

--- heather/main.py
+++ heather/main.py
@@ -1,17 +1,17 @@
 # anita.heather.main.py
 from _spy.vitollino.main import Cena, Elemento, Texto, STYLE
 
-botao = "https://i.imgur.com/UarK8yh.png"
+
 
 def Historia():
-	introd = Cena (img = "https://i.imgur.com/zSyNvrj.png")
+	introd = Cena (img = "https://i.imgur.com/Py9oguB.jpg")
     
-	botao = Elemento (img = "botao_iniciar_jogo",
+	botao = Elemento (img = "https://i.imgur.com/GGVktQI.png",
 	tit="Jogar",
-	style=dict(left=180, top=50,  Width=60, height=50))
+	style=dict(left=50, top=50,  Width=50, height=50))
      
-    	botao.entra(introd)
-        introd.vai()
-        botao.vai()
+	botao.entra(introd)
+	introd.vai()
+	botao.vai()
 	
 Historia()
