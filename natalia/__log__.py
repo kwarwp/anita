232db68f532a4834ad608d453016520d0c0fb8a1
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-18 11:23:39 -0300

    Created natalia/__log__.py @2019-11-18 14:23

---
+++ natalia/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Mon Nov 18 2019 11:23:41.775 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 10
+  quartodatalita = Cena (img="https://i.imgur.com/RCRUtAf.jpg"
+                                                                    ^
+SyntaxError: invalid syntax
+'''},
