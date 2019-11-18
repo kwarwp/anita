debbd489a95ebf564fdef03d25311fb3f988f435
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-17 23:40:05 -0300

    Created heather/__log__.py @2019-11-18 02:40

---
+++ heather/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Sun Nov 17 2019 23:40:03.418 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 8
+  intro.vai()
+  ^
+IndentationError: unexpected indent
+'''},
