4fb63113529c01efa9efc89e490c9fae299c11e9
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-17 23:40:12 -0300

    Saved heather/__log__.py @2019-11-18 02:40

--- heather/__log__.py
+++ heather/__log__.py
@@ -6,3 +6,10 @@
   ^
 IndentationError: unexpected indent
 '''},
+{'date': 'Sun Nov 17 2019 23:40:12.449 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 8
+  intro.vai()
+  ^
+IndentationError: unexpected indent
+'''},
