416a3de41663f632ef952561998568f575817e46
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-17 23:03:34 -0300

    Saved rachel/__log__.py @2019-11-18 02:03

--- rachel/__log__.py
+++ rachel/__log__.py
@@ -623,3 +623,21 @@ NameError: name 'quartotalita' is not defined
     quartotalita.direita = sala
 NameError: name 'quartotalita' is not defined
 '''},
+{'date': 'Sun Nov 17 2019 23:03:34.30 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 41
+    Historia()
+  module <module> line 34
+    self.florzinha = Elemento(FOCO, x=0, y=350, cena=self.quartotalita,
+NameError: name 'FOCO' is not defined
+'''},
