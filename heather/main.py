99416c5b0dd02006c74fd7edcf3d83932df44159
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-18 07:12:45 -0300

    Saved heather/main.py @2019-11-18 10:12

--- heather/main.py
+++ heather/main.py
@@ -5,12 +5,13 @@ botao = "https://i.imgur.com/UarK8yh.png"
 
 def Historia():
 	introd = Cena (img = "https://i.imgur.com/zSyNvrj.png")
-	botao = Elemento (img = "botao_iniciar_jogo", 
-				tit="Jogar",
-                       style=dict(left=50, top=250,  Width=50, height=200))
-     
     
-	botao.entra(introd)
-	introd.vai()
+	botao = Elemento (img = "botao_iniciar_jogo",
+	tit="Jogar",
+	style=dict(left=180, top=50,  Width=60, height=50))
+     
+    	botao.entra(introd)
+        introd.vai()
+        botao.vai()
 	
 Historia()
