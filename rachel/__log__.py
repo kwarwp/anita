5c50d9337650254cc1b4e851d23c16ae0edfffbc
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-17 23:06:51 -0300

    Saved rachel/__log__.py @2019-11-18 02:06

--- rachel/__log__.py
+++ rachel/__log__.py
@@ -713,3 +713,21 @@ NameError: name 'FOCO' is not defined
     self.florzinha = Elemento(FOCO, x=0, y=350, cena=self.quartotalita,
 NameError: name 'FOCO' is not defined
 '''},
+{'date': 'Sun Nov 17 2019 23:06:49.969 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 41
+    Historia()
+  module <module> line 34
+    florzinha = Elemento(FOCO, x=50, y=300, cena=quartotalita,
+NameError: name 'FOCO' is not defined
+'''},
