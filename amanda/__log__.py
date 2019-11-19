f402e83cc0b8e2e1bd1d016263e97adec30831aa
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-19 03:57:18 -0300

    Saved amanda/__log__.py @2019-11-19 06:57

--- amanda/__log__.py
+++ amanda/__log__.py
@@ -216,3 +216,19 @@ IndentationError: unexpected indent
                                                                           ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Tue Nov 19 2019 03:57:16.28 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 3
+    STYLE[wilth]=600 
+NameError: name 'wilth' is not defined
+'''},
