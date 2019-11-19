9b8d302ecb171ee318a560e419554faea01d33df
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-19 03:53:25 -0300

    Saved amanda/main.py @2019-11-19 06:53

--- amanda/main.py
+++ amanda/main.py
@@ -9,7 +9,7 @@ def jogo():
 
 	quartodatalita = Cena (img = "https://i.imgur.com/MXoIGJR.jpg")
 			talita = Elemento (img = linkdatalita,
-                 tit="talita",
+			tit="talita",
 			style=dict(left=30, top=10, width=10, heih=10))
 			coletedatalita = Elemento (img = "https://i.imgur.com/Pkho3lb.jpg")
 			tit="colete",
