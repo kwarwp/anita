37eab66f633cd10969e071520f341a65b1d8d12e
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-19 03:41:33 -0300

    Saved amanda/__log__.py @2019-11-19 06:41

--- amanda/__log__.py
+++ amanda/__log__.py
@@ -174,3 +174,10 @@ IndentationError: unexpected indent
   ^
 IndentationError: unexpected indent
 '''},
+{'date': 'Tue Nov 19 2019 03:41:31.583 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 11
+  talita = Elemento (img = "https://img2.gratispng.com/20180623/kjj/kisspng-figurine-doll-cartoon-character-costume-cry-girl-5b2eca836b23a0.0029348415297931554389.jpg" )  
+  ^
+IndentationError: unexpected indent
+'''},
