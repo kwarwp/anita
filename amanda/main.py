901e3e1b2ccd9084582d0e3e621e1b3a8510b547
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-19 03:33:41 -0300

    Saved amanda/main.py @2019-11-19 06:33

--- amanda/main.py
+++ amanda/main.py
@@ -8,17 +8,12 @@ linkcoletedatalita="https://i.imgur.com/q7R5gdc.jpg"
 def jogo():
 
 	quartodatalita = Cena (img = "https://i.imgur.com/MXoIGJR.jpg")
-                     tit="quarto",
-                     style=dict(left=80.top=25,width=30,heih=30))
 	coletedatalita = Elemento (img = "https://i.imgur.com/Pkho3lb.jpg")
-                       tit="colete",
-                       style=dict(lef=15,top=15,width=10,heih=20))
-	talita = Elemento (img = 
-			 tit="talita",
-			 style=dict(left=180.top=50,width=60,heih=50))
-            
-            
-talita.entra(quartodatalita)
-colete.entra(quartodatalita)
-quartodatalita.vai
-jogo():
+			tit="colete",
+			style=dict(lef=15,top=15,width=10,heih=20))
+			talita= Elemento (img = "https://img2.gratispng.com/20180623/kjj/kisspng-figurine-doll-cartoon-character-costume-cry-girl-5b2eca836b23a0.0029348415297931554389.jpg" 
+			tit="talita"            
+                 style=dict(left=30, top=10, width=10, heih=10))
+	talita.entra(quartodatalita)
+	colete.entra(quartodatalita)
+	quartodatalita.vai
