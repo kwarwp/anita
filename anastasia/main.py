b19a406526c79a4e656f2de582be4fb0c439b000
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-19 05:10:02 -0300

    Saved anastasia/main.py @2019-11-19 08:10

--- anastasia/main.py
+++ anastasia/main.py
@@ -24,9 +24,8 @@ def Historia():
       	livro.entra(quartotalita)
          
         
-		textotalita = Texto (quartotalita, "Oi, eu sou a Talita, preciso de sua ajuda nessa aventura! Me ajude a achar objetos fabricados por mulheres.")
-    	textocolete = Texto (quartotalita, "Stephanie Kwolek criou o colete à prova de balas Kevlar, que todos os anos salva a vida de milhares de policiais.")
-        textolivro = Texto (quartotalita,"O livro mágico poderá te ajudar a passar em algumas fases do jogo.")
-        talita.vai = textotalita.vai
-        colete.vai = textocolete.vai
-        protetorsolar.vai = textoprotetor.vai
+textotalita = Texto (quartotalita, "Oi, eu sou a Talita, preciso de sua ajuda nessa aventura! Me ajude a achar objetos fabricados por mulheres.")
+textocolete = Texto (quartotalita, "Stephanie Kwolek criou o colete à prova de balas Kevlar, que todos os anos salva a vida de milhares de policiais.") textolivro = Texto (quartotalita,"O livro mágico poderá te ajudar a passar em algumas fases do jogo.")
+talita.vai = textotalita.vai
+colete.vai = textocolete.vai
+protetorsolar.vai = textoprotetor.vai
