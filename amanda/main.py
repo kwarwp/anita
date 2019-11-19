39befb076ce47d77d3abe6846a59ae637cfb8519
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-19 03:53:49 -0300

    Saved amanda/main.py @2019-11-19 06:53

--- amanda/main.py
+++ amanda/main.py
@@ -11,6 +11,7 @@ def jogo():
 			talita = Elemento (img = linkdatalita,
 			tit="talita",
 			style=dict(left=30, top=10, width=10, heih=10))
+            
 			coletedatalita = Elemento (img = "https://i.imgur.com/Pkho3lb.jpg")
 			tit="colete",
 			style=dict(lef=15,top=15,width=10,heih=20))
