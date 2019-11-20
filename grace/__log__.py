4de78421a850c4bbb7213b57f2bcf7613fdfb3da
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-20 17:19:10 -0300

    Saved grace/__log__.py @2019-11-20 20:19

--- grace/__log__.py
+++ grace/__log__.py
@@ -98,3 +98,19 @@ IndentationError: unexpected indent
     textodatalita = Texto (quartotalita, "Oi, eu sou a Talita, preciso de sua ajuda nessa aventura! Me ajude a achar objetos fabricados por mulheres.")
 NameError: name 'quartotalita' is not defined
 '''},
+{'date': 'Wed Nov 20 2019 17:19:08.516 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 30
+    textodatalita = Texto (quartodatalita, "Oi, eu sou a Talita, preciso de sua ajuda nessa aventura! Me ajude a achar objetos fabricados por mulheres.")
+NameError: name 'quartodatalita' is not defined
+'''},
