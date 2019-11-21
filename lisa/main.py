6b98f44f6a91555bc49e449a1481118dbc7b5d32
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-21 20:09:28 -0300

    Saved lisa/main.py @2019-11-21 23:09

--- lisa/main.py
+++ lisa/main.py
@@ -4,7 +4,6 @@ STYLE ["width"]=600
 STYLE ["height"]= "200px"
 linkdatalita="https://imagensemoldes.com.br/wp-content/uploads/2018/03/Bonecas-LOL-Serie-2-Cosplay-Club-Midnight-PNG-220x300.png"
 linkdocolete="https://i.imgur.com/mbj8tzc.png"
-linkcoisa1="https://i.dlpng.com/static/png/5431677-gray-sunscreen-gray-sunscreen-a-bottle-png-and-vector-with-sunscreen-png-650_651_preview.png"
 def Jogo ():
     quartodatalita= Cena (img= "https://image.freepik.com/vetores-gratis/modelo-de-plano-de-fundo-interior-quarto-dos-desenhos-animados-aconchegante-casa-moderna-sala-na-luz-da-manha_33099-171.jpg")
     talita = Elemento (img =linkdatalita,
@@ -14,9 +13,6 @@ def Jogo ():
     colete = Elemento (img= linkdocolete,
     			  tit="colete",
                 	 style=dict (left=50,top=250,width=50,height=200))
-    coisa1 = Elemento (img= linkcoisa1,
-                       tit="protetorsolar",
-                       style=dict (left=25,top=20,width=35,height=25))
                     
     talita.entra(quartodatalita)
     colete.entra(quartodatalita)
