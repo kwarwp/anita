f3cd186b1d2bd9c9ed1010a4b3dd9b1050861a7d
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-22 06:36:26 -0300

    Saved kristen/main.py @2019-11-22 09:36

--- kristen/main.py
+++ kristen/main.py
@@ -31,7 +31,7 @@ def Jogo():
       tit="escrivaninha",
       style= dict(left=50, top=250, width=50, heigth=80))
       
-	bola = Elemento (img = linkbola,
+	bola = Elemento (img = linkdabola,
       tit="bola",
       style= dict(left=20, top=250, widt=50, heigth=90))
                          
