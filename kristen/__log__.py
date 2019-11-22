f8d36cac00ced70a79038737a4a7ca8bcddea373
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-22 06:12:35 -0300

    Saved kristen/__log__.py @2019-11-22 09:12

--- kristen/__log__.py
+++ kristen/__log__.py
@@ -337,3 +337,21 @@ IndentationError: unexpected indent
                                                                                                       ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Fri Nov 22 2019 06:12:33.557 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 40
+    Jogo()
+  module <module> line 37
+    	livros.vai=textolivros.vai
+NameError: name 'textolivros' is not defined
+'''},
