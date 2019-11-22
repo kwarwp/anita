0c193f7aa1b832c4591e809262eec90cb9faaa82
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-22 09:32:40 -0300

    Saved natalia/main.py @2019-11-22 12:32

--- natalia/main.py
+++ natalia/main.py
@@ -8,17 +8,17 @@ linkdocolete = "https://i.imgur.com/IZkZR1d.jpg"
 linkdachave = "https://i.imgur.com/Jwdhb9P.jpg"
 linkdapanelinha = "https://i.imgur.com/HCb4RvU.jpg"
 def jogo (): 
-	quartodatalita = Cena (img = "https://i.imgur.com/RCRUtAf.jpg" 
-	talita = Elemento (img="linkdatalita")
-    			   tit = "talita"
+	quartodatalita = Cena (img = "https://i.imgur.com/RCRUtAf.jpg") 
+	talita = Elemento (img="linkdatalita",
+    			   tit = "talita",
 			       style = dict(left=180, top=50,  Width=60, height=50))
-    colete = Elemento (img="linkdocolete")
+    colete = Elemento (img="linkdocolete",
     			tit = "colete"
                      style = dict(left=140, top= 30, width= 20, height=10)
-    chave= Elemento (img= "linkdachave")
+    chave= Elemento (img= "linkdachave",
     			tit = "chave"
                      style = dict(left=90, top= 18,  width= 15, height= 6)
-    panelinha= Elemento (img= "linkdapanelinha")
+    panelinha= Elemento (img= "linkdapanelinha",
     			tit= "panelinha"
                      style = dict(left= 5, top= 3, width= 4, height=2)
                      
