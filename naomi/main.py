051da0cbb7c4c2fb777ce1b853ea07a7994b0e82
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-22 09:38:22 -0300

    Saved naomi/main.py @2019-11-22 12:38

--- naomi/main.py
+++ naomi/main.py
@@ -9,7 +9,7 @@ def Jogo():
 	quartodatalita=Cena(img="https://i.imgur.com/DEC5m3T.jpg")
 	talita= Elemento (img= linkdatalita,
                      tit="talita",
-                     style=dict(left=180, top=30, width=80, heith=30))
+                     style=dict(left=100, top=10, width=30, heith=10))
      
 	colete= Elemento(img=linkcolete,
 	tit="colete",
