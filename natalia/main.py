c12e9bc49a450b7f5dec2fc5e2ccbc75153277a4
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-22 10:22:39 -0300

    Saved natalia/main.py @2019-11-22 13:22

--- natalia/main.py
+++ natalia/main.py
@@ -4,26 +4,25 @@ STYLE["wildth"]= 600
 STYLE["heigt"]= "200px" 
 linkdatalita= "https://www.trzcacak.rs/myfile/detail/201-2017902_bonecas-png-menina-paris.png"
 linkdocolete= "https://i.imgur.com/IZkZR1d.jpg"
-
 linkdachave= "https://i.imgur.com/Jwdhb9P.jpg"
 linkdapanelinha= "https://i.imgur.com/HCb4RvU.jpg"
 def jogo (): 
 	quartodatalita=Cena(img = "https://i.imgur.com/RCRUtAf.jpg") 
 	talita= Elemento (img= linkdatalita,
-    			   tit = "talita",
-			       style = dict(left=180, top=50,  Width=60, height=50))
+                        tit = "talita"
+                        style = dict(left=180, top=50,  Width=60, height=50))
                    
   	colete= Elemento(img= linkdocolete,
-      tit = "colete",
-      style = dict(left=140, top= 30, width= 20, height=10))
+                       tit = "colete",
+                       style = dict(left=120, top= 30, width= 40, height=30))
                       
-	chave= Elemento (img= linkdachave,
-      tit = "chave",
-      style = dict(left=90, top= 18,  width= 15, height= 6))
+	chave= Elemento(img= linkdachave,
+                      tit = "chave",
+                      style = dict(left=90, top= 18,  width= 15, height= 10))
                      
 	panelinha= Elemento (img= linkdapanelinha,
-      tit= "panelinha",
-      style = dict(left= 5, top= 3, width= 4, height=2))
+                          tit= "panelinha",
+                          style = dict(left= 10, top= 15, width= 10, height=10))
       
                      
 	talita.entra(quartodatalita)
