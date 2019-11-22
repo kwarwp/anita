21a56659ecbbc1b027c4f2d88c96292ae7dbe9bc
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-22 10:02:33 -0300

    Saved amanda/main.py @2019-11-22 13:02

--- amanda/main.py
+++ amanda/main.py
@@ -12,7 +12,7 @@ def Jogo():
 			tit="talita",
 			style=dict(left=30, top=10, width=10, heih=10))
             
-	coletedatalita = Elemento (img = linkdocolete,
+	coletedatalita = Elemento (img = linkcoletetalita,
 			 tit="colete",
 			 style=dict(lef=15, top=15, width=10, heih=20))
 
