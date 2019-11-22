b23c7417d3c14b337d11534e314621e217de8883
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-22 10:23:32 -0300

    Saved lisa/main.py @2019-11-22 13:23

--- lisa/main.py
+++ lisa/main.py
@@ -4,15 +4,20 @@ STYLE ["width"]=600
 STYLE ["height"]= "200px"
 linkdatalita="https://imagensemoldes.com.br/wp-content/uploads/2018/03/Bonecas-LOL-Serie-2-Cosplay-Club-Midnight-PNG-220x300.png"
 linkdocolete="https://i.imgur.com/mbj8tzc.png"
+linkdabarbie="https://i.imgur.com/O8Xlq5o.jpg"
 def Jogo ():
-    quartodatalita= Cena (img= "https://image.freepik.com/vetores-gratis/modelo-de-plano-de-fundo-interior-quarto-dos-desenhos-animados-aconchegante-casa-moderna-sala-na-luz-da-manha_33099-171.jpg")
-    talita = Elemento (img =linkdatalita,
+	quartodatalita= Cena (img= "https://image.freepik.com/vetores-gratis/modelo-de-plano-de-fundo-interior-quarto-dos-desenhos-animados-aconchegante-casa-moderna-sala-na-luz-da-manha_33099-171.jpg")
+	talita = Elemento (img =linkdatalita,
 				tit="talita",
 				style=dict (left=300,top=50,width=60,height=50))
                      
-    colete = Elemento (img= linkdocolete,
+				colete = Elemento (img= linkdocolete,
     			  tit="colete",
-                	 style=dict (left=50,top=250,width=50,height=200))
+				 style=dict (left=50,top=250,width=50,height=200))
+
+			       barbie= Elemento (img= linkdabarbie,
+                        tit= "barbie",
+                        style=dict (left=50,top=250,width=50,height=200))
                     
     talita.entra(quartodatalita)
     colete.entra(quartodatalita)
