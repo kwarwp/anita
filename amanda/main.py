be6a61d4737e4e65126098974b1faf3aecfb4c47
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-22 10:17:15 -0300

    Saved amanda/main.py @2019-11-22 13:17

--- amanda/main.py
+++ amanda/main.py
@@ -1,6 +1,6 @@
 # anita.amanda.main.py
 from _spy.vitollino.main import Cena,Elemento,Texto,STYLE
-STYLE["width"]= 300
+STYLE["width"]= 500
 STYLE["heigh"]="200px"
 linkdatalita="https://img2.gratispng.com/20180623/kjj/kisspng-figurine-doll-cartoon-character-costume-cry-girl-5b2eca836b23a0.0029348415297931554389.jpg"
 linkquartodatalita="https://www.dhresource.com/600x600/f2/albu/g5/M00/9D/73/rBVaI1ipR1qAWzKcAAacXdBLd0Y328.jpg"
