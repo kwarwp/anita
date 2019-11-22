de41b05b04f75714d7a63c6c03ee81b7dd01dbfc
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-22 09:31:19 -0300

    Saved natalia/__log__.py @2019-11-22 12:31

--- natalia/__log__.py
+++ natalia/__log__.py
@@ -13,3 +13,10 @@ SyntaxError: invalid syntax
                                                                       ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Fri Nov 22 2019 09:31:18.600 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 11
+  quartodatalita = Cena (img = "https://i.imgur.com/RCRUtAf.jpg" 
+                                                                    ^
+SyntaxError: invalid syntax
+'''},
