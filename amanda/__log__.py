e988ee65a51a8ab2fb7ae1c7bdb397ee5d7cf11b
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-22 09:17:40 -0300

    Saved amanda/__log__.py @2019-11-22 12:17

--- amanda/__log__.py
+++ amanda/__log__.py
@@ -264,3 +264,19 @@ NameError: name 'wilth' is not defined
     STYLE[wilth]= 600 
 NameError: name 'wilth' is not defined
 '''},
+{'date': 'Fri Nov 22 2019 09:17:49.33 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 3
+    STYLE[wilth]= 600 
+NameError: name 'wilth' is not defined
+'''},
