b5ba2f44c33d1840731e2aedd7b62fd5eb4c3aec
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-22 10:17:06 -0300

    Saved natalia/main.py @2019-11-22 13:17

--- natalia/main.py
+++ natalia/main.py
@@ -8,19 +8,23 @@ linkdocolete= "https://i.imgur.com/IZkZR1d.jpg"
 linkdachave= "https://i.imgur.com/Jwdhb9P.jpg"
 linkdapanelinha= "https://i.imgur.com/HCb4RvU.jpg"
 def jogo (): 
-	quartodatalita = Cena (img = "https://i.imgur.com/RCRUtAf.jpg") 
+	quartodatalita=Cena(img = "https://i.imgur.com/RCRUtAf.jpg") 
 	talita= Elemento (img= linkdatalita,
     			   tit = "talita",
 			       style = dict(left=180, top=50,  Width=60, height=50))
-  	colete= Elemento (img= linkdocolete,
-    			 tit = "colete",
-                      style = dict(left=140, top= 30, width= 20, height=10))
+                   
+  	colete= Elemento(img= linkdocolete,
+      tit = "colete",
+      style = dict(left=140, top= 30, width= 20, height=10))
+                      
 	chave= Elemento (img= linkdachave,
-    			tit = "chave",
-                     style = dict(left=90, top= 18,  width= 15, height= 6))
+      tit = "chave",
+      style = dict(left=90, top= 18,  width= 15, height= 6))
+                     
 	panelinha= Elemento (img= linkdapanelinha,
-    			   tit= "panelinha",
-                        style = dict(left= 5, top= 3, width= 4, height=2))
+      tit= "panelinha",
+      style = dict(left= 5, top= 3, width= 4, height=2))
+      
                      
 	talita.entra(quartodatalita)
 	colete.entra(quartodatalita)
@@ -31,5 +35,7 @@ def jogo ():
 	colete.vai
 	chave.vai
 	panelinha.vai
+    
 	quartodatalita.vai()
+    
 jogo()
