5c599b2fc94538c7927263e95dc26dfd77379f51
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-22 09:43:45 -0300

    Saved natalia/main.py @2019-11-22 12:43

--- natalia/main.py
+++ natalia/main.py
@@ -15,10 +15,10 @@ def jogo ():
   	colete = Elemento (img= linkdocolete,
     			 tit = "colete",
                       style = dict(left=140, top= 30, width= 20, height=10))
-    chave= Elemento (img= linkdachave,
+	chave= Elemento (img= linkdachave,
     			tit = "chave"
                      style = dict(left=90, top= 18,  width= 15, height= 6)
-    panelinha= Elemento (img= linkdapanelinha,
+	panelinha= Elemento (img= linkdapanelinha,
     			   tit= "panelinha"
                         style = dict(left= 5, top= 3, width= 4, height=2))
                      
