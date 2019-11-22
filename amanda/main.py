6d72ed98a79aed69ed9c4fc4bf106cbc750f43b0
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-22 10:18:41 -0300

    Saved amanda/main.py @2019-11-22 13:18

--- amanda/main.py
+++ amanda/main.py
@@ -4,7 +4,7 @@ STYLE["width"]= 500
 STYLE["heigh"]="200px"
 linkdatalita="https://img2.gratispng.com/20180623/kjj/kisspng-figurine-doll-cartoon-character-costume-cry-girl-5b2eca836b23a0.0029348415297931554389.jpg"
 linkquartodatalita="https://www.dhresource.com/600x600/f2/albu/g5/M00/9D/73/rBVaI1ipR1qAWzKcAAacXdBLd0Y328.jpg"
-linkcoletedatalita="https://i.imgur.com/q7R5gdc.jpg"
+linkcoletedatalita="https://i.imgur.com/MhoUxzF.jpg"
 def Jogo():
 
 	quartodatalita=Cena (img = "https://i.imgur.com/MXoIGJR.jpg")
