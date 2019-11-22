0fa42407e02bd843aa98eb7a25a8bdfb6ee26d51
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-22 09:48:34 -0300

    Saved natalia/__log__.py @2019-11-22 12:48

--- natalia/__log__.py
+++ natalia/__log__.py
@@ -83,3 +83,10 @@ SyntaxError: invalid syntax
                                                                                 ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Fri Nov 22 2019 09:48:33.706 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 31
+  colete.vai
+  ^
+IndentationError: unexpected indent
+'''},
