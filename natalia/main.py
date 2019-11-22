67e35fdf86e0dc9e3edbf648685d672883453b47
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-22 09:31:16 -0300

    Saved natalia/main.py @2019-11-22 12:31

--- natalia/main.py
+++ natalia/main.py
@@ -4,13 +4,14 @@ STYLE ["wilth"]= 600
 STYLE["heigt"]= "200" 
 linkdatalita = "https://www.trzcacak.rs/myfile/detail/201-2017902_bonecas-png-menina-paris.png"
 linkdocolete = "https://i.imgur.com/IZkZR1d.jpg"
+
 linkdachave = "https://i.imgur.com/Jwdhb9P.jpg"
 linkdapanelinha = "https://i.imgur.com/HCb4RvU.jpg"
 def jogo (): 
-    quartodatalita = Cena (img = "https://i.imgur.com/RCRUtAf.jpg"
-    talita = Elemento (img="linkdatalita")
-    			tit = "talita"
-                     style = dict(left=180, top=50,  Width=60, height=50))
+	quartodatalita = Cena (img = "https://i.imgur.com/RCRUtAf.jpg" 
+	talita = Elemento (img="linkdatalita")
+    			   tit = "talita"
+			       style = dict(left=180, top=50,  Width=60, height=50))
     colete = Elemento (img="linkdocolete")
     			tit = "colete"
                      style = dict(left=140, top= 30, width= 20, height=10)
@@ -20,6 +21,7 @@ def jogo ():
     panelinha= Elemento (img= "linkdapanelinha")
     			tit= "panelinha"
                      style = dict(left= 5, top= 3, width= 4, height=2)
+                     
 	talita.entra(quartodatalita)
 	colete.entra(quartodatalita)
 	chave.entra(quartodatalita)
