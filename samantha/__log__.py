2a64af6453b8c018d6d9d5cc4b428f3ae3bc56f0
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-24 14:53:29 -0300

    Saved samantha/__log__.py @2019-11-24 17:53

--- samantha/__log__.py
+++ samantha/__log__.py
@@ -1172,3 +1172,21 @@ IndentationError: expected an indented block
     textocolete = Texto (quartotalita, "Stephanie Kwolek criou o colete à prova de balas Kevlar, que todos os anos salva a vida de milhares de policiais").ganhamoeda()
 AttributeError: 'Texto' object has no attribute 'ganhamoeda'
 '''},
+{'date': 'Sun Nov 24 2019 14:53:28.504 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 66
+    Historia()
+  module <module> line 48
+    colete.vai = textocolete.vai
+AttributeError: 'NoneType' object has no attribute 'vai'
+'''},
