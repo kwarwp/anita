9060211ac0c59d91a2717a4471aad30deedd473f
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-24 14:28:42 -0300

    Saved samantha/__log__.py @2019-11-24 17:28

--- samantha/__log__.py
+++ samantha/__log__.py
@@ -911,3 +911,10 @@ SyntaxError: invalid syntax
             ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Sun Nov 24 2019 14:28:42.147 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 53
+  if colete.vai ==true
+                       ^
+SyntaxError: invalid syntax
+'''},
