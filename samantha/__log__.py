629562ae3a4fbee6f363ff8db6238ad1b1b52a66
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-24 15:35:52 -0300

    Saved samantha/__log__.py @2019-11-24 18:35

--- samantha/__log__.py
+++ samantha/__log__.py
@@ -1574,3 +1574,10 @@ UnboundLocalError: local variable 'submarino' referenced before assignment
   ^
 IndentationError: unexpected indent
 '''},
+{'date': 'Sun Nov 24 2019 15:35:51.980 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 73
+  submarino.vai()
+  ^
+IndentationError: expected an indented block
+'''},
