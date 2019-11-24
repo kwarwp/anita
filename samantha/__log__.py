109263c46b33e395341885f322d785ae983b49c6
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-24 20:55:18 -0300

    Saved samantha/__log__.py @2019-11-24 23:55

--- samantha/__log__.py
+++ samantha/__log__.py
@@ -2864,3 +2864,19 @@ NameError: name 'talita' is not defined
     escolatalita = Elemento(FOCO, x=150, y=150, w=50, h=50, cena=maparegiao, style={"opacity": 0}, vai=escola.vai)
 NameError: name 'escola' is not defined
 '''},
+{'date': 'Sun Nov 24 2019 20:55:17.821 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 32
+    textotravesseiro = Texto (quartotalita, "Não era bem isso que eu estava procurando..")  
+NameError: name 'quartotalita' is not defined
+'''},
