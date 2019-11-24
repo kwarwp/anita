9f16fb85fb940031572aefa1ad41b1790bec65c4
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-24 16:21:56 -0300

    Saved samantha/main.py @2019-11-24 19:21

--- samantha/main.py
+++ samantha/main.py
@@ -75,7 +75,7 @@ talita = Elemento (img = linkdatalita,
                        style=dict(left=250, top=400, width=120, heigth=1500))
 talita.entra(submarino)
 
-textoquadropicasso = Texto (submarino, "Este quadro é de Pablo Picasso e chama-se Tête de femme au chapeau")  
+	textoquadropicasso = Texto (submarino, "Este quadro é de Pablo Picasso e chama-se Tête de femme au chapeau")  
 	textoquadropicasso = Elemento(FOCO, x=90, y=180, w=50, h=50, cena=submarino, style={"opacity": 0.5}, vai=textoquadropicasso.vai)
 
     
