2962f141cf0ee387092a823466d84c0ec969cd70
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-24 15:06:31 -0300

    Saved samantha/__log__.py @2019-11-24 18:06

--- samantha/__log__.py
+++ samantha/__log__.py
@@ -1269,3 +1269,21 @@ IndentationError: unexpected indent
     textocolete = Texto (quartotalita, "Stephanie Kwolek criou o colete à prova de balas Kevlar, que todos os anos salva a vida de milhares de policiais").ganharmoeda.vai()
 AttributeError: 'Texto' object has no attribute 'ganharmoeda'
 '''},
+{'date': 'Sun Nov 24 2019 15:06:30.379 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 71
+    Historia()
+  module <module> line 50
+    textocolete.vai = ganharmoeda.vai
+UnboundLocalError: local variable 'ganharmoeda' referenced before assignment
+'''},
