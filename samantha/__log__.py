1b039a80df58212b0a2ee32e47f69a6f534a685c
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-24 13:57:42 -0300

    Saved samantha/__log__.py @2019-11-24 16:57

--- samantha/__log__.py
+++ samantha/__log__.py
@@ -507,3 +507,10 @@ IndentationError: unexpected indent
   ^
 IndentationError: unexpected indent
 '''},
+{'date': 'Sun Nov 24 2019 13:57:41.569 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 25
+  fc = inicia()
+  ^
+IndentationError: unexpected indent
+'''},
