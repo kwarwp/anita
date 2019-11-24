1d18421e9b2bf8df84e4dbf11922482e1b6e1e54
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-24 19:57:23 -0300

    Saved samantha/__log__.py @2019-11-24 22:57

--- samantha/__log__.py
+++ samantha/__log__.py
@@ -2300,3 +2300,19 @@ SyntaxError: non-keyword arg after keyword arg
     cenamoeda.esquerda= quartotalita.vai
 NameError: name 'quartotalita' is not defined
 '''},
+{'date': 'Sun Nov 24 2019 19:57:22.326 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 104
+    cenamoeda.esquerda= quartotalita.vai
+NameError: name 'quartotalita' is not defined
+'''},
