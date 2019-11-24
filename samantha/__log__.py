0afdd895ee895e38827e590dd7167e1c251c4efd
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-24 13:42:38 -0300

    Saved samantha/__log__.py @2019-11-24 16:42

--- samantha/__log__.py
+++ samantha/__log__.py
@@ -243,3 +243,10 @@ AttributeError: 'Quarto' object has no attribute 'inicia'
   ^
 IndentationError: unexpected indent
 '''},
+{'date': 'Sun Nov 24 2019 13:42:38.353 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 31
+  fc = Quarto()
+  ^
+IndentationError: unexpected indent
+'''},
