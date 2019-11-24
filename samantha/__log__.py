75ec1b77581a750b1b1f83c4fa4551f966fa8ea4
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-24 14:45:32 -0300

    Saved samantha/__log__.py @2019-11-24 17:45

--- samantha/__log__.py
+++ samantha/__log__.py
@@ -1108,3 +1108,21 @@ SyntaxError: EOL while scanning string literal
   ^
 IndentationError: unexpected indent
 '''},
+{'date': 'Sun Nov 24 2019 14:45:31.230 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 67
+    Historia()
+  module <module> line 49
+    colete.vai = textocolete.vai
+AttributeError: 'NoneType' object has no attribute 'vai'
+'''},
