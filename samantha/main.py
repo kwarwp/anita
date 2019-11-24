70fcee693000cec41b7e3bf30fe571e59678d3fe
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-24 17:02:12 -0300

    Saved samantha/main.py @2019-11-24 20:02

--- samantha/main.py
+++ samantha/main.py
@@ -84,7 +84,7 @@ quadroportinari = Elemento(FOCO, x=240, y=200, w=100, h=100, cena=submarino, sty
 textoquadrotarsila = Texto (submarino, "Este quadro se chama Abaporu. É de Tarsila do Amaral, uma grande pintora brasileira.")  
 quadrotarsila = Elemento(FOCO, x=380, y=260, w=50, h=50, cena=submarino, style={"opacity": 0.0}, vai=textoquadrotarsila.vai)
 
-if textoquadrotarsila.vai==true:
+if quadrotarsila.vai:
 	Texto(submarino, "parabens, voce ganhou uma moeda").vai()
 
 Historia()
