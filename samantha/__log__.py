af51af94d836629c978b9b3620f380159eb4a553
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-24 13:41:48 -0300

    Saved samantha/__log__.py @2019-11-24 16:41

--- samantha/__log__.py
+++ samantha/__log__.py
@@ -220,3 +220,19 @@ IndentationError: unexpected indent
     	fc.inicia()
 AttributeError: 'Quarto' object has no attribute 'inicia'
 '''},
+{'date': 'Sun Nov 24 2019 13:41:47.140 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 32
+    	fc.inicia()
+AttributeError: 'Quarto' object has no attribute 'inicia'
+'''},
