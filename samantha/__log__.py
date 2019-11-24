89e19f8d20447be1f16ffc71fe86aa2965dfc69b
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-24 19:24:00 -0300

    Saved samantha/__log__.py @2019-11-24 22:23

--- samantha/__log__.py
+++ samantha/__log__.py
@@ -2261,3 +2261,19 @@ SyntaxError: non-keyword arg after keyword arg
     textoquadrotarsila = moeda.entra(submarino)
 NameError: name 'moeda' is not defined
 '''},
+{'date': 'Sun Nov 24 2019 19:23:59.81 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 86
+    quadrotarsila = Elemento(FOCO, x=380, y=260, w=50, h=50, cena=submarino, style={"opacity": 0.0}, vai=moeda.vai)
+NameError: name 'moeda' is not defined
+'''},
