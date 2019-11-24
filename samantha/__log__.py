a7d8a879a6dddf591c062299428a88418233cd33
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-24 13:22:52 -0300

    Saved samantha/__log__.py @2019-11-24 16:22

--- samantha/__log__.py
+++ samantha/__log__.py
@@ -6,3 +6,10 @@
   ^
 IndentationError: unexpected indent
 '''},
+{'date': 'Sun Nov 24 2019 13:22:51.659 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 51
+  Jogo.moedas -=1
+  ^
+IndentationError: expected an indented block
+'''},
