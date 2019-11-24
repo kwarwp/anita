4c04398c10ac80f6f3141c47e67047a71b197141
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-24 13:22:27 -0300

    Created samantha/__log__.py @2019-11-24 16:22

---
+++ samantha/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Sun Nov 24 2019 13:22:25.938 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 49
+  def pegamoeda(self):
+  ^
+IndentationError: unexpected indent
+'''},
