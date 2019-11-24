6cd892a8b16db863d19f8774c2f6751c261f89af
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-24 15:55:16 -0300

    Saved samantha/__log__.py @2019-11-24 18:55

--- samantha/__log__.py
+++ samantha/__log__.py
@@ -1808,3 +1808,21 @@ AttributeError: 'submarino' object has no attribute 'vai'
     	sala.direita = submarino.vai()
 AttributeError: 'submarino' object has no attribute 'vai'
 '''},
+{'date': 'Sun Nov 24 2019 15:55:15.450 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 75
+    Historia()
+  module <module> line 69
+    	sala.direita = submarino
+UnboundLocalError: local variable 'submarino' referenced before assignment
+'''},
