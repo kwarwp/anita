ff09844a819c779cbe24ff42558d63b3bbf660c5
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-24 15:58:31 -0300

    Saved samantha/main.py @2019-11-24 18:58

--- samantha/main.py
+++ samantha/main.py
@@ -72,7 +72,7 @@ def Historia():
 submarino = Cena (img = "https://i.imgur.com/GOH738j.jpg")
 talita = Elemento (img = linkdatalita, 
 				tit="talita",
-                       style=dict(left=180, top=120,  Width=60, height=50))
+                       style=dict(left=180, top=120,  Width=60, height=100))
 talita.entra(submarino)
     
 Historia()
