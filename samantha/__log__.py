ad0b1bd0067097209abffc7dc3ae8f91a162cdf3
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-24 14:08:33 -0300

    Saved samantha/__log__.py @2019-11-24 17:08

--- samantha/__log__.py
+++ samantha/__log__.py
@@ -713,3 +713,21 @@ IndentationError: unexpected indent
     	travesseiro = Elemento(FOCO, x=70, y=400, w=50, h=50, cena=quartotalita, style={"opacity": 0.3}, vai=self._travesseiro)
 AttributeError: 'GUI' object has no attribute '_travesseiro'
 '''},
+{'date': 'Sun Nov 24 2019 14:08:32.446 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 58
+    Historia()
+  module <module> line 19
+    	travesseiro = Elemento(FOCO, x=70, y=400, w=50, h=50, cena=quartotalita, style={"opacity": 0.3}, vai=travesseiro)
+UnboundLocalError: local variable 'travesseiro' referenced before assignment
+'''},
