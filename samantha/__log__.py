afa536253bf07e0619377efd0edaefd0c6341e8d
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-24 15:26:03 -0300

    Saved samantha/__log__.py @2019-11-24 18:26

--- samantha/__log__.py
+++ samantha/__log__.py
@@ -1330,3 +1330,10 @@ AttributeError: 'function' object has no attribute 'vai'
   ^
 IndentationError: expected an indented block
 '''},
+{'date': 'Sun Nov 24 2019 15:26:03.95 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 72
+  talita.entra(submarino)
+  ^
+IndentationError: expected an indented block
+'''},
