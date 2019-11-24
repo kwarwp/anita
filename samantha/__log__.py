e18b0aad4dc76ee9546016d8a51f025104f1f4cb
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-24 20:54:59 -0300

    Saved samantha/__log__.py @2019-11-24 23:54

--- samantha/__log__.py
+++ samantha/__log__.py
@@ -2848,3 +2848,19 @@ IndentationError: unexpected indent
     talita.entra(escola)
 NameError: name 'talita' is not defined
 '''},
+{'date': 'Sun Nov 24 2019 20:54:58.913 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 25
+    escolatalita = Elemento(FOCO, x=150, y=150, w=50, h=50, cena=maparegiao, style={"opacity": 0}, vai=escola.vai)
+NameError: name 'escola' is not defined
+'''},
