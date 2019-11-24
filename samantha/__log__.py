eedc3b0f32fcafca12ce664701148673366eac4f
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-24 20:48:00 -0300

    Saved samantha/__log__.py @2019-11-24 23:48

--- samantha/__log__.py
+++ samantha/__log__.py
@@ -2669,3 +2669,19 @@ AttributeError: 'bool' object has no attribute 'vai'
                  ^
 SyntaxError: can't assign to function call 
 '''},
+{'date': 'Sun Nov 24 2019 20:47:59.710 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 73
+    submarino.vai=textosubmarino.vai()
+AttributeError: 'bool' object has no attribute 'vai'
+'''},
