c3b1494a6aa8e86fea725201ad879369a634704b
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-24 14:23:22 -0300

    Saved samantha/__log__.py @2019-11-24 17:23

--- samantha/__log__.py
+++ samantha/__log__.py
@@ -827,3 +827,10 @@ IndentationError: unexpected indent
   ^
 IndentationError: unexpected indent
 '''},
+{'date': 'Sun Nov 24 2019 14:23:21.739 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 53
+  if colete.vai = textocolete.vai
+  ^
+IndentationError: expected an indented block
+'''},
