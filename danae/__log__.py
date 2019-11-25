1fb85495c822ab557eb34482983d9ad4622ce2a9
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-25 09:56:32 -0300

    Saved danae/__log__.py @2019-11-25 12:56

--- danae/__log__.py
+++ danae/__log__.py
@@ -295,3 +295,21 @@ AttributeError: 'Jogo' object has no attribute 'quarto'
     			self.botao.vai=quarto.vai()
 TypeError: vai() missing 1 positional argument: self
 '''},
+{'date': 'Mon Nov 25 2019 02:56:36.671 GMt-1000 (Horário Padrão do Havaí e Ilhas Aleutas) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 38
+    Jogo()
+  module <module> line 24
+    			self.botao.vai=quarto.vai()
+TypeError: vai() missing 1 positional argument: self
+'''},
