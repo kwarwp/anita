a26ad1bcd322e7c5b4f0e9e50e47a7e3eaf07fc2
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-25 14:17:57 -0300

    Saved danae/__log__.py @2019-11-25 17:17

--- danae/__log__.py
+++ danae/__log__.py
@@ -971,3 +971,21 @@ IndentationError: unexpected indent
     			Elemento (COLETE, vai=self.habilita)
 NameError: name 'COLETE' is not defined
 '''},
+{'date': 'Mon Nov 25 2019 07:18:01.648 GMt-1000 (Horário Padrão do Havaí e Ilhas Aleutas) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 56
+    	Jogo()    
+  module <module> line 33
+    			Elemento (self.colete, vai=self.habilita)
+AttributeError: 'Jogo' object has no attribute 'habilita'
+'''},
