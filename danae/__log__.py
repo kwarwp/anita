ba176056973a3f8d6710850dfa15a0c7ee1a2808
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-25 13:59:09 -0300

    Saved danae/__log__.py @2019-11-25 16:59

--- danae/__log__.py
+++ danae/__log__.py
@@ -892,3 +892,21 @@ AttributeError: 'Jogo' object has no attribute 'sala'
     			self.colete = Elemento (img = linkcolete, vai=sala.vai(),
 UnboundLocalError: local variable 'sala' referenced before assignment
 '''},
+{'date': 'Mon Nov 25 2019 06:59:13.793 GMt-1000 (Horário Padrão do Havaí e Ilhas Aleutas) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 55
+    	Jogo()    
+  module <module> line 27
+    			self.colete = Elemento (img = linkcolete, vai=self.sala,
+AttributeError: 'Jogo' object has no attribute 'sala'
+'''},
