031d6aed11238d86dea82ffd38676aa7964d4028
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-25 12:37:25 -0300

    Saved danae/__log__.py @2019-11-25 15:37

--- danae/__log__.py
+++ danae/__log__.py
@@ -609,3 +609,21 @@ UnboundLocalError: local variable 'habilita' referenced before assignment
     			self.colete = Elemento (img = linkcolete, vai=habilita,
 UnboundLocalError: local variable 'habilita' referenced before assignment
 '''},
+{'date': 'Mon Nov 25 2019 05:37:26.390 GMt-1000 (Horário Padrão do Havaí e Ilhas Aleutas) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 52
+    	Jogo()    
+  module <module> line 32
+    			self.colete.habilita(sala.vai)
+AttributeError: 'Elemento' object has no attribute 'habilita'
+'''},
