9786a86afd0139c53afab8dc0bcd8dace15acea3
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-24 21:11:00 -0300

    Saved samantha/__log__.py @2019-11-25 00:11

--- samantha/__log__.py
+++ samantha/__log__.py
@@ -2919,3 +2919,19 @@ NameError: name 'sala' is not defined
   ^
 IndentationError: unexpected indent
 '''},
+{'date': 'Sun Nov 24 2019 21:10:59.645 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 109
+    senasotao.direita = fimdojogo.vai()
+NameError: name 'senasotao' is not defined
+'''},
