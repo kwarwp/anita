31977d683e1c1c357940f222602f0995bbf9aa53
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-25 15:00:37 -0300

    Saved meredith/__log__.py @2019-11-25 18:00

--- meredith/__log__.py
+++ meredith/__log__.py
@@ -316,3 +316,21 @@ NameError: name 'protetorsolar' is not defined
     	travesseiro.entra(quartodaTalita)
 NameError: name 'travesseiro' is not defined
 '''},
+{'date': 'Mon Nov 25 2019 15:00:48.754 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 30
+    Jogo()
+  module <module> line 28
+    	protetorsolar.vai = textoprotetor.vai
+NameError: name 'protetorsolar' is not defined
+'''},
