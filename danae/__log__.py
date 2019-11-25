cb6557f7e9bb1dc29bf1d07b550b9a523a8ae7b3
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-25 08:54:08 -0300

    Saved danae/__log__.py @2019-11-25 11:54

--- danae/__log__.py
+++ danae/__log__.py
@@ -6,3 +6,10 @@
   ^
 IndentationError: expected an indented block
 '''},
+{'date': 'Mon Nov 25 2019 01:54:13.390 GMt-1000 (Horário Padrão do Havaí e Ilhas Aleutas) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 13
+  def quarto():
+  ^
+IndentationError: expected an indented block
+'''},
