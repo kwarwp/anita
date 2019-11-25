430bcbd670c26b87a17e8e7a697dec8fd8f02262
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-25 08:55:10 -0300

    Saved danae/main.py @2019-11-25 11:55

--- danae/main.py
+++ danae/main.py
@@ -12,21 +12,21 @@ class Jogo:
 
 	def quarto():
 		introd = Cena (img = "https://i.imgur.com/Bcnfg0C.png")
-		quartotalita = Cena (img = "https://lh5.googleusercontent.com/-fs1hatHWU9s/UUpy0DJqlXI/AAAAAAAAbs4/Vy1LL28sPeY/s400/tumblr_lt4n2aSjsX1qmvaoo.gif")
+	quartotalita = Cena (img = "https://lh5.googleusercontent.com/-fs1hatHWU9s/UUpy0DJqlXI/AAAAAAAAbs4/Vy1LL28sPeY/s400/tumblr_lt4n2aSjsX1qmvaoo.gif")
     
- 		talita = Elemento (img = linkdatalita, 
+ 	talita = Elemento (img = linkdatalita, 
 				tit="talita",
                        style=dict(left=180, top=120,  Width=60, height=50))
      
-		textotravesseiro = Texto (quartotalita, "Não era bem isso que eu estava procurando..")  
-		travesseiro = Elemento(FOCO, x=90, y=180, w=50, h=50, cena=quartotalita, style={"opacity": 0.0}, vai=textotravesseiro.vai)
+	textotravesseiro = Texto (quartotalita, "Não era bem isso que eu estava procurando..")  
+	travesseiro = Elemento(FOCO, x=90, y=180, w=50, h=50, cena=quartotalita, style={"opacity": 0.0}, vai=textotravesseiro.vai)
 	
 	
 	   
-		colete = Elemento (img = linkcolete,
-		tit = "colete",
-		style=dict(left=300, top=140, width=50, heigth=80))
+	colete = Elemento (img = linkcolete,
+	tit = "colete",
+	style=dict(left=300, top=140, width=50, heigth=80))
     
-		protetorsolar = Elemento (img = linkprotetorsolar,
-    	tit = "protetorsolar",
-     	style = dict(left=50, top=250, width=50, height=200))
+	protetorsolar = Elemento (img = linkprotetorsolar,
+	tit = "protetorsolar",
+	style = dict(left=50, top=250, width=50, height=200))
