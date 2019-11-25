23742d6417834d06986e7a34d1be0073f224d496
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-25 11:08:38 -0300

    Saved meredith/main.py @2019-11-25 14:08

--- meredith/main.py
+++ meredith/main.py
@@ -2,6 +2,8 @@
 from _spy.vitollino.main import Cena,Elemento,Texto,STYLE
 STYLE["width"]= 600
 STYLE["heigh"]="200px"
+linkdacapadojogo="https://i.imgur.com/NwcgtMe.jpg"
+linkdobotao="https://i.imgur.com/Gtv8FsB.jpg"  
 linkdaTalita="https://www.google.com/search?biw=1536&bih=754&tbm=isch&sa=1&ei=sVTJXZ-HF7S_5OUPybSyqA0&q=menina+desenho+desendentes+jpg&oq=menina+desenho+desendentes+jpg&gs_l=img.3...313613.315755..317248...0.0..0.551.1408.0j1j0j2j0j1......0....1..gws-wiz-img.KTaxQa_aeII&ved=0ahUKEwjfo76EleLlAhW0H7kGHUmaDNUQ4dUDCAc&uact=5#imgdii=S1LzO66A9prhcM:&imgrc=9H6EQNxskC89fM:"
 linKquartodaTalita="https://st3.depositphotos.com/8065304/15935/v/1600/depositphotos_159354828-stock-illustration-the-interior-of-the-bedroom.jpg"
 linkcoletedaTalita="https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwj3ssW-neLlAhUfGLkGHd2jCyQQjRx6BAgBEAQ&url=https%3A%2F%2Ffardasdelite.com%2Fpt%2Fcolete-a-prova-de-balas&psig=AOvVaw2yF6ItL3CQjMtTAPoFLUY0&ust=1573563884838448https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=08&ved=2ahUKEwj3ssW-neLlAhUfGLkGHd2jCyQQjRx6BAgBEAQ&url=https%3A%2F%2Ffardasdelite.com%2Fpt%2Fcolete-a-prova-de-balas&psig=AOvVaw2yF6ItL3CQjMtTAPoFLUY0&ust=1573563884838448"
