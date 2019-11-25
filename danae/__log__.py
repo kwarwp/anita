ee5730be0dd12241dbd6dcfa7fb38c4d3c70f3a6
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-25 14:33:36 -0300

    Saved danae/__log__.py @2019-11-25 17:33

--- danae/__log__.py
+++ danae/__log__.py
@@ -1061,3 +1061,10 @@ AttributeError: 'Jogo' object has no attribute 'habilita'
     			Elemento (self.colete, vai=self.habilita)
 AttributeError: 'Jogo' object has no attribute 'habilita'
 '''},
+{'date': 'Mon Nov 25 2019 07:33:41.245 GMt-1000 (Horário Padrão do Havaí e Ilhas Aleutas) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 15
+  self.introd = Cena (img = "https://lh5.googleusercontent.com/-fs1hatHWU9s/UUpy0DJqlXI/AAAAAAAAbs4/Vy1LL28sPeY/s400/tumblr_lt4n2aSjsX1qmvaoo.gif")
+  ^
+IndentationError: expected an indented block
+'''},
