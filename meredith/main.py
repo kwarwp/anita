264992631e3a7a875ed72fcaca040d674c7506cf
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-25 14:54:12 -0300

    Saved meredith/main.py @2019-11-25 17:54

--- meredith/main.py
+++ meredith/main.py
@@ -6,28 +6,28 @@ linkdacapadojogo="https://i.imgur.com/NwcgtMe.jpg"
 linkdobotao="https://i.imgur.com/gZTKZhv.png"  
 linkdaTalita="https://i.imgur.com/qSWjvPE.png"
 linKquartodaTalita="https://i.imgur.com/DyTUlGG.jpg"
+linkprotetorsolar = "https://i.imgur.com/1ph5Tne.png"
 def jogo():
-	quartodaTalita=Cena(img="linkquartoTalita")
-                    tit("quartodaTalita"
-                    style=dict(left=180, top=50, witdh=60, heith=50))
-	coletedaTalita=Elemento(img="linkcoletedaTalita")
-                     tit("coletedaTalita"
-                     style=dict(left=170, top=40, witdh=50, heith=40))
+	quartodaTalita=Cena(img=linkquartoTalita)
+	
+	colete = Elemento (img = linkcolete,
+		tit = "colete",
+		style=dict(left=300, top=140, width=50, heigth=80))
                               
 	talita = Elemento (img = linkdatalita, 
 				tit="talita",
                        style=dict(left=180, top=120,  Width=60, height=50))
 
-talita.entra(quartotalita)
-     	colete.entra(quartotalita)
-        protetorsolar.entra(quartotalita)
-        travesseiro.entra(quartotalita)
-        textotravesseiro = Texto (quartotalita, "Não era bem isso que eu estava procurando..")    
-        textotalita = Texto (quartotalita, "Olá. Hoje vai ser um dia longo e eu preciso estar preparada para encarar muitos desafios. Hoje sairei de Costa Barros protegida e contarei com uma invenção femina para isso.")
-        textocolete = Texto (quartotalita, "Stephanie Kwolek criou o colete à prova de balas Kevlar, que todos os anos salva a vida de milhares de policiais")
-        textoprotetor = Texto (quartotalita,"O protetor solar é muito importante, mas não é o que estou procurando.")
-        talita.vai = textotalita.vai
-        colete.vai = textocolete.vai
-        protetorsolar.vai = textoprotetor.vai
+talita.entra(quartodaTalita)
+colete.entra(quartodaTalita)
+protetorsolar.entra(quartodaTalita)
+travesseiro.entra(quartodaTalita)
+textotravesseiro = Texto (quartodaTalita, "Não era bem isso que eu estava procurando..")    
+textotalita = Texto (quartodaTalita, "Olá. Hoje vai ser um dia longo e eu preciso estar preparada para encarar muitos desafios. Hoje sairei de Costa Barros protegida e contarei com uma invenção femina para isso.")
+textocolete = Texto (quartodaTalita, "Stephanie Kwolek criou o colete à prova de balas Kevlar, que todos os anos salva a vida de milhares de policiais")
+textoprotetor = Texto (quartodaTalita,"O protetor solar é muito importante, mas não é o que estou procurando.")
+talita.vai = textotalita.vai
+colete.vai = textocolete.vai
+protetorsolar.vai = textoprotetor.vai
         
 Jogo()
