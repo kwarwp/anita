3cba49d161fc67375a7eb36b516aaadfba2ec5db
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-25 10:38:15 -0300

    Saved danae/__log__.py @2019-11-25 13:38

--- danae/__log__.py
+++ danae/__log__.py
@@ -363,3 +363,19 @@ AttributeError: 'GUI' object has no attribute 'talita'
     self.talita.entra(introd)
 AttributeError: 'GUI' object has no attribute 'talita'
 '''},
+{'date': 'Mon Nov 25 2019 03:38:19.766 GMt-1000 (Horário Padrão do Havaí e Ilhas Aleutas) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 34
+    talita.entra(introd)
+NameError: name 'talita' is not defined
+'''},
