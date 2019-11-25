5d255b562ca2770e6b4f49ee669cf9b7029877eb
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-25 10:50:45 -0300

    Saved danae/__log__.py @2019-11-25 13:50

--- danae/__log__.py
+++ danae/__log__.py
@@ -466,3 +466,21 @@ IndentationError: expected an indented block
     			self.colete = Elemento (vai=self.habilita)
 AttributeError: 'Jogo' object has no attribute 'habilita'
 '''},
+{'date': 'Mon Nov 25 2019 03:50:49.409 GMt-1000 (Horário Padrão do Havaí e Ilhas Aleutas) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 56
+    	Jogo()    
+  module <module> line 28
+    			self.colete = Elemento (vai=self.habilita)
+AttributeError: 'Jogo' object has no attribute 'habilita'
+'''},
