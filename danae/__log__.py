42455c8aab0f0be3d4e973654823b86ed63def2b
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-25 15:12:03 -0300

    Saved danae/__log__.py @2019-11-25 18:12

--- danae/__log__.py
+++ danae/__log__.py
@@ -1110,3 +1110,10 @@ IndentationError: expected an indented block
   ^
 IndentationError: expected an indented block
 '''},
+{'date': 'Mon Nov 25 2019 08:12:05.802 GMt-1000 (Horário Padrão do Havaí e Ilhas Aleutas) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 14
+  self.introd = Cena (img = "https://lh5.googleusercontent.com/-fs1hatHWU9s/UUpy0DJqlXI/AAAAAAAAbs4/Vy1LL28sPeY/s400/tumblr_lt4n2aSjsX1qmvaoo.gif")
+  ^
+IndentationError: expected an indented block
+'''},
