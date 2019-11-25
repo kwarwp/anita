b671713d6a25184801bea0c5bd78b97e24d00737
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-25 14:18:59 -0300

    Saved danae/__log__.py @2019-11-25 17:18

--- danae/__log__.py
+++ danae/__log__.py
@@ -1007,3 +1007,21 @@ AttributeError: 'Jogo' object has no attribute 'habilita'
     			Elemento (colete, vai=self.habilita)
 NameError: name 'colete' is not defined
 '''},
+{'date': 'Mon Nov 25 2019 07:19:03.917 GMt-1000 (Horário Padrão do Havaí e Ilhas Aleutas) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 57
+    	Jogo()    
+  module <module> line 33
+    			Elemento (self.colete, vai=self.habilita)
+AttributeError: 'Jogo' object has no attribute 'habilita'
+'''},
