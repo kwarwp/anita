70c35fa10d5ad43a706ffe13c5d0e69b82077054
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-25 13:11:50 -0300

    Saved danae/__log__.py @2019-11-25 16:11

--- danae/__log__.py
+++ danae/__log__.py
@@ -730,3 +730,21 @@ IndentationError: unexpected indent
   ^
 IndentationError: unexpected indent
 '''},
+{'date': 'Mon Nov 25 2019 06:11:49.874 GMt-1000 (Horário Padrão do Havaí e Ilhas Aleutas) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 55
+    	Jogo()    
+  module <module> line 27
+    			self.colete = Elemento (img = linkcolete, vai=habilita(),
+TypeError: habilita() missing 2 positional arguments: self,evento
+'''},
