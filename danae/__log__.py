0fd8dba29c5b2b35c14e4c15ab2ee8413aef6d8b
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-25 08:52:53 -0300

    Created danae/__log__.py @2019-11-25 11:52

---
+++ danae/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Mon Nov 25 2019 01:52:56.364 GMt-1000 (Horário Padrão do Havaí e Ilhas Aleutas) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 14
+  introd = Cena (img = "https://i.imgur.com/Bcnfg0C.png")
+  ^
+IndentationError: expected an indented block
+'''},
