4bff51e47255fe629ccea00d9ad21124d3861e8a
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-25 14:43:54 -0300

    Saved meredith/__log__.py @2019-11-25 17:43

--- meredith/__log__.py
+++ meredith/__log__.py
@@ -6,3 +6,10 @@
   ^
 IndentationError: unexpected indent
 '''},
+{'date': 'Mon Nov 25 2019 14:44:05.387 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 11
+  tit("quartodaTalita"
+  ^
+IndentationError: unexpected indent
+'''},
