0de5ad22ed5aebf8e63866fbe7b3ec4739c8c0a7
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-25 17:22:49 -0300

    Saved danae/main.py @2019-11-25 20:22

--- danae/main.py
+++ danae/main.py
@@ -13,23 +13,20 @@ class Jogo:
     def __init__(self):
         self.introd = Cena (img = "https://lh5.googleusercontent.com/-fs1hatHWU9s/UUpy0DJqlXI/AAAAAAAAbs4/Vy1LL28sPeY/s400/tumblr_lt4n2aSjsX1qmvaoo.gif")
 
-    sala = Cena (img = "https://i.imgur.com/Q57lw3T.jpg")
-
-    def habilita(self,evento):
-        self.introd.direita=self.sala        
+#    sala = Cena (img = "https://i.imgur.com/Q57lw3T.jpg") esqueceu o self! idendação errada!!
+        self.sala = Cena (img = "https://i.imgur.com/Q57lw3T.jpg")
+        self.textotravesseiro = Texto (self.introd, "Não era bem isso que eu estava procurando..")  
+        self.travesseiro = Elemento(FOCO, x=90, y=180, w=50, h=50, cena=self.introd, style={"opacity": 0.0}, vai=self.textotravesseiro.vai)
 
         self.talita = Elemento (img = linkdatalita, 
             tit="talita",
                        style=dict(left=180, top=120,  Width=60, height=50))
-     
-        self.textotravesseiro = Texto (self.introd, "Não era bem isso que eu estava procurando..")  
-        self.travesseiro = Elemento(FOCO, x=90, y=180, w=50, h=50, cena=self.introd, style={"opacity": 0.0}, vai=self.textotravesseiro.vai)
 
         self.colete = Elemento (img = linkcolete,
         tit = "colete",
-        style=dict(left=300, top=140, width=50, heigth=80))
+        style=dict(left=300, top=140, width=50, heigth=80), vai=self.habilita)
 
-        Elemento (self.colete, vai=self.habilita)
+        # Elemento (self.colete, vai=self.habilita) esta linha está perdida,  vai=self.habilita no element acima
             
         self.protetorsolar = Elemento (img = linkprotetorsolar,
         tit = "protetorsolar",
@@ -47,6 +44,42 @@ class Jogo:
         self.talita.vai = self.textotalita.vai
         self.colete.vai = self.textocolete.vai
         self.protetorsolar.vai = self.textoprotetor.vai
+
+    def habilita(self, evento):
+        self.introd.direita=self.sala        
+                       
+        """ tudo isso tinha que estar no __init__
+
+        self.talita = Elemento (img = linkdatalita, 
+            tit="talita",
+                       style=dict(left=180, top=120,  Width=60, height=50))
+
+        self.textotravesseiro = Texto (self.introd, "Não era bem isso que eu estava procurando..")  
+        self.travesseiro = Elemento(FOCO, x=90, y=180, w=50, h=50, cena=self.introd, style={"opacity": 0.0}, vai=self.textotravesseiro.vai)
+
+        self.colete = Elemento (img = linkcolete,
+        tit = "colete",
+        style=dict(left=300, top=140, width=50, heigth=80), vai=self.habilita)
+
+        # Elemento (self.colete, vai=self.habilita) esta linha está perdida,  vai=self.habilita no element acima
+            
+        self.protetorsolar = Elemento (img = linkprotetorsolar,
+        tit = "protetorsolar",
+        style = dict(left=50, top=250, width=50, height=200))
+        
+        self.introd.vai()
+        self.talita.entra(self.introd)
+        self.colete.entra(self.introd)
+        self.protetorsolar.entra(self.introd)
+
+        self.textotravesseiro = Texto (self.introd, "Não era bem isso que eu estava procurando..")    
+        self.textotalita = Texto (self.introd, "Olá. Hoje vai ser um dia longo e eu preciso estar preparada para encarar muitos desafios. Hoje sairei de Costa Barros protegida e contarei com uma invenção femina para isso.")
+        self.textocolete = Texto (self.introd, "Stephanie Kwolek criou o colete à prova de balas Kevlar, que todos os anos salva a vida de milhares de policiais")
+        self.textoprotetor = Texto (self.introd,"O protetor solar é muito importante, mas não é o que estou procurando.")
+        self.talita.vai = self.textotalita.vai
+        self.colete.vai = self.textocolete.vai
+        self.protetorsolar.vai = self.textoprotetor.vai
+        """
 
 
 if __name__ == "__main__":
