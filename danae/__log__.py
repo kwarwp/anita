fbdd1f566231fd85156aab13cf590e07a48f0ce5
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-25 09:49:13 -0300

    Saved danae/__log__.py @2019-11-25 12:49

--- danae/__log__.py
+++ danae/__log__.py
@@ -259,3 +259,21 @@ TypeError: inicia() missing 1 positional argument: self
     self.quarto.inicia()
 AttributeError: 'GUI' object has no attribute 'quarto'
 '''},
+{'date': 'Mon Nov 25 2019 02:49:17.835 GMt-1000 (Horário Padrão do Havaí e Ilhas Aleutas) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 31
+    Jogo()
+  module <module> line 17
+    			self.introd.vai=self.quarto.vai()
+AttributeError: 'Jogo' object has no attribute 'quarto'
+'''},
