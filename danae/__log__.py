cd00e470f040ddf5ec22e3905949a0ffa3bd83c2
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-25 12:58:57 -0300

    Saved danae/__log__.py @2019-11-25 15:58

--- danae/__log__.py
+++ danae/__log__.py
@@ -709,3 +709,10 @@ NameError: name 'habilita' is not defined
   ^
 IndentationError: expected an indented block
 '''},
+{'date': 'Mon Nov 25 2019 05:59:02.341 GMt-1000 (Horário Padrão do Havaí e Ilhas Aleutas) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 51
+  self.sala.vai
+  ^
+IndentationError: unexpected indent
+'''},
