6adc386e2057ddba14974f205a92dd7b1153c327
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-25 14:58:01 -0300

    Saved meredith/__log__.py @2019-11-25 17:58

--- meredith/__log__.py
+++ meredith/__log__.py
@@ -226,3 +226,21 @@ NameError: name 'linkquartoTalita' is not defined
     	quartodaTalita=Cena(img=linkquartoTalita)
 NameError: name 'linkquartoTalita' is not defined
 '''},
+{'date': 'Mon Nov 25 2019 14:58:13.29 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 33
+    Jogo()
+  module <module> line 11
+    	quartodaTalita=Cena(img=linkquartodaTalita)
+NameError: name 'linkquartodaTalita' is not defined
+'''},
