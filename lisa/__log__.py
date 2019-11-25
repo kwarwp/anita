25cab3e6fdff49c5c108c7e818f1c6313f58b3b4
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-25 13:19:05 -0300

    Saved lisa/__log__.py @2019-11-25 16:19

--- lisa/__log__.py
+++ lisa/__log__.py
@@ -525,3 +525,10 @@ IndentationError: unexpected indent
   ^
 IndentationError: unexpected indent
 '''},
+{'date': 'Mon Nov 25 2019 13:19:05.185 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 20
+  colete = Elemento (img= linkdocolete ,
+  ^
+IndentationError: unexpected indent
+'''},
