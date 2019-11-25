af18f6f3db050a2d7b7fb9cd728cbf0849615fe1
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-25 08:54:22 -0300

    Saved danae/__log__.py @2019-11-25 11:54

--- danae/__log__.py
+++ danae/__log__.py
@@ -13,3 +13,10 @@ IndentationError: expected an indented block
   ^
 IndentationError: expected an indented block
 '''},
+{'date': 'Mon Nov 25 2019 01:54:26.962 GMt-1000 (Horário Padrão do Havaí e Ilhas Aleutas) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 22
+  travesseiro = Elemento(FOCO, x=90, y=180, w=50, h=50, cena=quartotalita, style={"opacity": 0.0}, vai=textotravesseiro.vai)
+  ^
+IndentationError: unexpected indent
+'''},
