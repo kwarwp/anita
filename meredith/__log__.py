feb78f8faeb8323ca88e6fe913e692ad87fa100c
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-25 14:43:32 -0300

    Created meredith/__log__.py @2019-11-25 17:43

---
+++ meredith/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Mon Nov 25 2019 14:43:41.689 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 11
+  tit("quartodaTalita"
+  ^
+IndentationError: unexpected indent
+'''},
