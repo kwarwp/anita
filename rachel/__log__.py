8511d95e104702a14d2952ff000775f5c9db9d02
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-26 10:34:27 -0300

    Saved rachel/__log__.py @2019-11-26 13:34

--- rachel/__log__.py
+++ rachel/__log__.py
@@ -906,3 +906,10 @@ IndentationError: unexpected indent
   ^
 IndentationError: unexpected indent
 '''},
+{'date': 'Tue Nov 26 2019 10:34:27.214 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 32
+  colete.entra(quartotalitatalita)
+  ^
+IndentationError: unexpected indent
+'''},
