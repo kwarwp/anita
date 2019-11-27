fb9dd5030a4ca62818fc6937203405070576b323
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-27 10:50:02 -0300

    Saved ruzwana/__log__.py @2019-11-27 13:50

--- ruzwana/__log__.py
+++ ruzwana/__log__.py
@@ -862,3 +862,21 @@ AttributeError: 'bool' object has no attribute 'nome'
     self.scorer = dict(ponto=1, valor=cena.nome, carta=tit or img, casa=self.xy, move=None)
 AttributeError: 'bool' object has no attribute 'nome'
 '''},
+{'date': 'Wed Nov 27 2019 10:50:01.587 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 118
+    Jogo() 
+  module <module> line 27
+    self.escola.direita = self.submarino
+AttributeError: 'Jogo' object has no attribute 'escola'
+'''},
