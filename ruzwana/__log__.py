dccce126b28262b0061eda60842e84de1b1d47a9
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-27 09:48:46 -0300

    Saved ruzwana/__log__.py @2019-11-27 12:48

--- ruzwana/__log__.py
+++ ruzwana/__log__.py
@@ -467,3 +467,21 @@ TypeError: (call) (id) $$super:((id) Texto:,(id) self:)() got multiple values fo
     self.textoquadrotarsila.foi = self.habilitaquadro
 AttributeError: 'Jogo' object has no attribute 'textoquadrotarsila'
 '''},
+{'date': 'Wed Nov 27 2019 09:48:45.483 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 100
+    Jogo() 
+  module <module> line 65
+    self.textoquadrotarsila.foi = self.habilitaquadro
+AttributeError: 'Jogo' object has no attribute 'textoquadrotarsila'
+'''},
