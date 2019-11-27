14445170d2671a8f38970ecef120930f3dbff396
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-27 15:11:15 -0300

    Saved ruzwana/__log__.py @2019-11-27 18:11

--- ruzwana/__log__.py
+++ ruzwana/__log__.py
@@ -1378,3 +1378,21 @@ AttributeError: 'Jogo' object has no attribute 'ganhamoeda3'
     self.ganhamoeda3.direita = self.voltasotao
 AttributeError: 'Jogo' object has no attribute 'ganhamoeda3'
 '''},
+{'date': 'Wed Nov 27 2019 15:11:14.587 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 187
+    Jogo() 
+  module <module> line 154
+    acha_painel_solar= Elemento (FOCO, x=0, y=0, w=200, h=100, cena=self.sotao, style={"opacity": 0.0}, vai=habilitavolta)
+NameError: name 'habilitavolta' is not defined
+'''},
