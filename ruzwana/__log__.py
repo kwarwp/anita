a652e359c2db619bff1cea8bcd4d01acc9db338f
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-27 15:08:23 -0300

    Saved ruzwana/__log__.py @2019-11-27 18:08

--- ruzwana/__log__.py
+++ ruzwana/__log__.py
@@ -1342,3 +1342,21 @@ AttributeError: 'Jogo' object has no attribute 'talitaencontrou'
     self.talitaencontrou = Elemento(FOCO, x=110, y=120, w=60, h=100, cena=self.voltasotao, style={"opacity": 0.5}, vai=self.talitaencontrou.vai)
 AttributeError: 'Jogo' object has no attribute 'talitaencontrou'
 '''},
+{'date': 'Wed Nov 27 2019 15:08:22.83 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 187
+    Jogo() 
+  module <module> line 167
+    self.ganhamoeda3.direita = self.voltasotao
+AttributeError: 'Jogo' object has no attribute 'ganhamoeda3'
+'''},
