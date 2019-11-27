0a8b85892450499b87bb3017f97c8907b6b14933
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-27 08:54:31 -0300

    Saved ruzwana/__log__.py @2019-11-27 11:54

--- ruzwana/__log__.py
+++ ruzwana/__log__.py
@@ -180,3 +180,23 @@ AttributeError: 'Jogo' object has no attribute 'sala'
 'error': '''
 NameError: <ReferenceError: no_set_attr is not defined>
 '''},
+{'date': 'Wed Nov 27 2019 08:54:29.965 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 99
+    Jogo() 
+  module <module> line 75
+    self.talita.entra(self.submarino)
+  module _spy.vitollino.main line 471
+    self.scorer.update(valor=cena.nome, move=self.xy,
+AttributeError: 'bool' object has no attribute 'nome'
+'''},
