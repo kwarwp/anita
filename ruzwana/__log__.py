c68643fe6492e6b11f97af1e4abcab413312640e
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-27 10:12:33 -0300

    Saved ruzwana/__log__.py @2019-11-27 13:12

--- ruzwana/__log__.py
+++ ruzwana/__log__.py
@@ -624,3 +624,10 @@ AttributeError: 'bool' object has no attribute 'nome'
     self.scorer.update(valor=cena.nome, move=self.xy,
 AttributeError: 'bool' object has no attribute 'nome'
 '''},
+{'date': 'Wed Nov 27 2019 10:12:33.241 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 95
+  self.submarino = Texto (self.submarino, "Neste submarino existem quadros de pintores importantes. Qual deles é de uma pintora?").vai()
+  ^
+IndentationError: unexpected indent
+'''},
