52498bfeffd235b3f11869cd613c6a242c034d9d
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-27 10:19:40 -0300

    Saved ruzwana/__log__.py @2019-11-27 13:19

--- ruzwana/__log__.py
+++ ruzwana/__log__.py
@@ -705,3 +705,23 @@ AttributeError: 'bool' object has no attribute 'nome'
   ^
 IndentationError: unexpected indent
 '''},
+{'date': 'Wed Nov 27 2019 10:19:38.719 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 112
+    Jogo() 
+  module <module> line 91
+    self.quadropicasso = Elemento(FOCO, x=100, y=200, w=100, h=100, cena=self.submarino, style={"opacity": 0.0}, vai=self.textoquadropicasso.vai)
+  module _spy.vitollino.main line 539
+    self.scorer = dict(ponto=1, valor=cena.nome, carta=tit or img, casa=self.xy, move=None)
+AttributeError: 'bool' object has no attribute 'nome'
+'''},
