01f30df8e9a318902f83134234d3601325dd8a36
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-27 15:01:13 -0300

    Saved ruzwana/__log__.py @2019-11-27 18:01

--- ruzwana/__log__.py
+++ ruzwana/__log__.py
@@ -1324,3 +1324,21 @@ AttributeError: 'Jogo' object has no attribute 'voltasotao'
     self.talitaencontrou = Elemento(FOCO, x=110, y=120, w=60, h=100, cena=self.voltasotao, style={"opacity": 0.0}, vai=self.talitaencontrou.vai)
 AttributeError: 'Jogo' object has no attribute 'talitaencontrou'
 '''},
+{'date': 'Wed Nov 27 2019 15:01:12.15 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 186
+    Jogo() 
+  module <module> line 168
+    self.talitaencontrou = Elemento(FOCO, x=110, y=120, w=60, h=100, cena=self.voltasotao, style={"opacity": 0.5}, vai=self.talitaencontrou.vai)
+AttributeError: 'Jogo' object has no attribute 'talitaencontrou'
+'''},
