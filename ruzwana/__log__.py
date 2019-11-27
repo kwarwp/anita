1825f880702074d1061905ed0911d585da1fd740
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-27 12:58:27 -0300

    Saved ruzwana/__log__.py @2019-11-27 15:58

--- ruzwana/__log__.py
+++ ruzwana/__log__.py
@@ -1115,3 +1115,21 @@ AttributeError: 'Jogo' object has no attribute 'livrohabilita'
     self.sitiodopicapauamarelo = Elemento(FOCO, x=90, y=120, w=20, h=20, cena=self.saladeaula, style={"opacity": 0.5}, vai=self.textositiodopicapauamarelo.vai)
 AttributeError: 'Jogo' object has no attribute 'textositiodopicapauamarelo'
 '''},
+{'date': 'Wed Nov 27 2019 12:58:26.406 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 138
+    Jogo() 
+  module <module> line 38
+    self.sitiodopicapauamarelo = Elemento(FOCO, x=90, y=120, w=20, h=20, cena=self.saladeaula, style={"opacity": 0.5}, vai=self.textositiodopicapauamarelo.vai)
+AttributeError: 'Jogo' object has no attribute 'textositiodopicapauamarelo'
+'''},
