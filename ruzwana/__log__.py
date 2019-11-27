01b9d847ffd52f2747e056e5f2e7abe73ab28fe0
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-27 13:00:23 -0300

    Saved ruzwana/__log__.py @2019-11-27 16:00

--- ruzwana/__log__.py
+++ ruzwana/__log__.py
@@ -1158,3 +1158,21 @@ IndentationError: unexpected indent
     self.ahoradaestrela = Elemento(FOCO, x=90, y=200, w=20, h=20, cena=self.saladeaula, style={"opacity": 0.5}, vai=self.ahoradaestrela.vai)
 AttributeError: 'Jogo' object has no attribute 'ahoradaestrela'
 '''},
+{'date': 'Wed Nov 27 2019 13:00:22.58 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 143
+    Jogo() 
+  module <module> line 51
+    self.ahoradaestrela = Elemento(FOCO, x=90, y=200, w=20, h=20, cena=self.saladeaula, style={"opacity": 0.5}, vai=textoahoradaestrela.vai)
+NameError: name 'textoahoradaestrela' is not defined
+'''},
