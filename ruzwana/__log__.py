b3b54713ba1359dbc1ace351fd5e0c0f48a310b5
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-27 14:58:05 -0300

    Saved ruzwana/__log__.py @2019-11-27 17:58

--- ruzwana/__log__.py
+++ ruzwana/__log__.py
@@ -1288,3 +1288,21 @@ NameError: <ReferenceError: no_set_attr is not defined>
     self.talitaencontrou = Texto (self.voltasotao, "OMG! Encontrei uma passagem secreta!")
 AttributeError: 'Jogo' object has no attribute 'voltasotao'
 '''},
+{'date': 'Wed Nov 27 2019 14:58:04.453 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 186
+    Jogo() 
+  module <module> line 166
+    self.talitaencontrou = Texto (self.voltasotao, "OMG! Encontrei uma passagem secreta!")
+AttributeError: 'Jogo' object has no attribute 'voltasotao'
+'''},
