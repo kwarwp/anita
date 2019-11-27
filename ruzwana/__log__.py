21c797b4da0f197a57c1599c2e4cd4b3430f5974
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-27 10:38:16 -0300

    Saved ruzwana/__log__.py @2019-11-27 13:38

--- ruzwana/__log__.py
+++ ruzwana/__log__.py
@@ -797,3 +797,21 @@ SyntaxError: invalid syntax
     self.ganhadiamante.esquerda = submarino
 NameError: name 'submarino' is not defined
 '''},
+{'date': 'Wed Nov 27 2019 10:38:15.109 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 117
+    Jogo() 
+  module <module> line 28
+    self.ganhadiamante.esquerda = submarino
+NameError: name 'submarino' is not defined
+'''},
