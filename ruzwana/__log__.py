d91f9c611af2a4265b70b9083ca05dd2d1b62685
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-27 09:56:38 -0300

    Saved ruzwana/__log__.py @2019-11-27 12:56

--- ruzwana/__log__.py
+++ ruzwana/__log__.py
@@ -492,3 +492,23 @@ AttributeError: 'Jogo' object has no attribute 'textoquadrotarsila'
                                                                                                                         ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Wed Nov 27 2019 09:56:37.50 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 103
+    Jogo() 
+  module <module> line 72
+    self.talita2.entra(self.submarino)
+  module _spy.vitollino.main line 471
+    self.scorer.update(valor=cena.nome, move=self.xy,
+AttributeError: 'bool' object has no attribute 'nome'
+'''},
