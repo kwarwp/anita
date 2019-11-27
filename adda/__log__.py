35b20796edbd6036a2c3a758fdd62c370914a6c0
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-27 06:56:16 -0300

    Saved adda/__log__.py @2019-11-27 09:56

--- adda/__log__.py
+++ adda/__log__.py
@@ -71,3 +71,21 @@ AttributeError: 'Cena' object has no attribute 'Texto'
     self.Texto(self.sala, "Está na hora de ir pra escola").vai()
 AttributeError: 'Jogo' object has no attribute 'Texto'
 '''},
+{'date': 'Wed Nov 27 2019 06:56:15.639 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 62
+    Jogo() 
+  module <module> line 18
+    Texto (sala, "Está na hora de ir pra escola").vai()
+NameError: name 'sala' is not defined
+'''},
