4a1a272380db7a06b2287c76c0d0c8aa7dddf641
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-27 17:34:53 -0300

    Saved ruzwana/main.py @2019-11-27 20:34

--- ruzwana/main.py
+++ ruzwana/main.py
@@ -17,6 +17,8 @@ class Jogo:
         self.sala = Cena (img = "https://i.imgur.com/Q57lw3T.jpg")
         self.maparegiao = Cena(img="https://i.imgur.com/MGJSDE3.png")
         self.ganhamoeda = Cena (img="https://i.imgur.com/koWP1dw.png")
+        self.ganhamoeda2 = Cena (img="https://i.imgur.com/koWP1dw.png")
+        self.ganhamoeda3 = Cena (img="https://i.imgur.com/koWP1dw.png")
         self.ganhamoeda.direita=self.sala
         self.ganhamoeda.esquerda=self.introd
         self.ganhadiamante = Cena (img="https://i.imgur.com/M9Xx8ab.png")
@@ -26,13 +28,16 @@ class Jogo:
         self.rampaescola = Cena (img="https://i.imgur.com/D9lGay5.jpg")
         self.saladeaula = Cena (img = "https://i.imgur.com/u3DYHmo.jpg")
         self.submarino = Cena (img="https://i.imgur.com/GOH738j.jpg")
-        self.saladeaula.direita = self.submarino
         self.saladeaula.esquerda = self.rampaescola
         self.rampaescola.esquerda = self.patioescola
-        self.ganhadiamante.esquerda = self.submarino
+        #self.ganhadiamante.esquerda = self.submarino
         self.painelsolar = Cena (img="https://i.imgur.com/5wYPsAJ.jpg")
         self.diamante = Elemento (FOCO, x=300, y=150, w=200, h=200, cena=self.ganhadiamante, style={"opacity": 0.0}, vai=self.painelsolar.vai)
-        
+        self.saladeaula2 = Cena (img="https://i.imgur.com/y2WMlxb.jpg")
+        self.sotao = Cena (img="https://i.imgur.com/D2KRRlS.jpg")
+        self.voltasotao = Cena ("https://i.imgur.com/SpyaFmX.jpg")
+        self.dentrodomuseu = Cena ("https://i.imgur.com/flkncLY.jpg")
+        self.final = Cena ("https://i.imgur.com/WkuaGWp.png")
 #cenaescola
         
         self.textositiodopicapauamarelo = Texto (self.saladeaula, "O sitio do pica-pau amarelo é uma importante obra de Monteiro Lobato!")
@@ -47,8 +52,12 @@ class Jogo:
         self.textooguarani = Texto (self.saladeaula, "Este é um romance histórico de José de Alencar")
         self.oguarani = Elemento(FOCO, x=110, y=120, w=60, h=100, cena=self.saladeaula, style={"opacity": 0.0}, vai=self.textooguarani.vai)
        
+        
+        self.ahoradaestrela = Elemento(FOCO, x=360, y=180, w=80, h=80, cena=self.saladeaula, style={"opacity": 0.0}, vai=self.habilitalivro)
         self.textoahoradaestrela = Texto (self.saladeaula, "A hora da estrela é uma importante obra de Clarice Lispector, grande escritora da Literatura brasileira.")
-        self.ahoradaestrela = Elemento(FOCO, x=360, y=180, w=80, h=80, cena=self.saladeaula, style={"opacity": 0.0}, vai=self.textoahoradaestrela.vai)
+       
+        self.ahoradaestrela.vai = self.textoahoradaestrela.vai
+        self.textoahoradaestrela.foi = self.habilitalivro
        
         self.talita3 = Elemento (img = "https://i.imgur.com/N5HXcxK.png", 
         tit="Talita",
@@ -110,17 +119,11 @@ class Jogo:
 
 #cena submarino
 
-       
-
-       
+        
         self.talita2 = Elemento (img = "https://i.imgur.com/N5HXcxK.png", 
         tit="talita",
         style=dict(left=250, top=400, width=120, heigth=1500))
         self.talita2.entra(self.submarino)
-        
-        
-        
-
         self.textoquadropicasso = Texto (self.submarino, "Este quadro é de Pablo Picasso e chama-se Tête de femme au chapeau")  
         self.quadropicasso = Elemento(FOCO, x=100, y=200, w=100, h=100, cena=self.submarino, style={"opacity": 0.0}, vai=self.textoquadropicasso.vai)
         self.textoquadroportinari = Texto (self.submarino, "Este quadro é de Portinari, de 1935 e chama-se café. Foi pintado com tinta a óleo.")  
@@ -129,21 +132,97 @@ class Jogo:
         self.textoquadrotarsila = Texto (self.submarino, "Este quadro se chama Abaporu. É de Tarsila do Amaral, uma grande pintora brasileira.")
         self.quadrotarsila.vai = self.textoquadrotarsila.vai
         self.textoquadrotarsila.foi = self.habilitaquadro
-        self.textotalita2 = Texto (self.submarino, "Neste submarino existe um quadro de uma pintora famosa. Me ajude a encontrar!")
+        self.textotalita2 = Texto (self.submarino, "A passagem secreta me trouxe até esse submarino e agora eu preciso encontrar o quadro de uma pintora brasileira famosa para conseguir sair!")
         self.talita2.vai = self.textotalita2.vai
         
-
+#cena museu
+        self.museu = Cena (img="https://i.imgur.com/aZPZEZo.png")
+        self.submarinomapa = Elemento(FOCO, x=290, y=150, w=180, h=50, cena=self.museu, style={"opacity": 0.0}, vai=self.dentrodomuseu.vai)
+        self.textosubmarinomapa = Texto (self.museu, "Já que o submarino me deixou aqui bem pertinho, vou entrar para conhecer o museu de Tecnologia.")
+        self.submarinomapa.direita = self.dentrodomuseu
         
+        self.talitamuseu = Elemento (img = "https://i.imgur.com/QV5fuXJ.png", 
+        tit="Talita no museu da Computação",
+        style=dict(left=430, top=220, width=120, heigth=1500))
+        
+        
+                  
+        self.textocharles = Texto (self.dentrodomuseu, "Esse é Charles Babbage. Ele inventou a primeira máquina analítica")
+        self.textocharles= Elemento (FOCO, x=0, y=50, w=150, h=150, cena=self.dentrodomuseu, style={"opacity": 0.0}, vai=self.textocharles.vai)
+        
+        self.textostevejobs = Texto (self.dentrodomuseu, "Esse é Steve Jobs. Ele foi o fundador da Apple")
+        self.textostevejobs= Elemento (FOCO, x=170, y=50, w=150, h=150, cena=self.dentrodomuseu, style={"opacity": 0.0}, vai=self.textostevejobs.vai)
+        
+        self.textotim = Texto (self.dentrodomuseu, "Esse é Tim Berners-Lee. Ele inventou a World Wide Web e é conhecido como o pai da internet.")
+        self.textotim= Elemento (FOCO, x=320, y=50, w=100, h=100, cena=self.dentrodomuseu, style={"opacity": 0.0}, vai=self.textotim.vai)
+        
+        self.ada= Elemento (FOCO, x=430, y=100, w=50, h=80, cena=self.dentrodomuseu, style={"opacity": 0.0}, vai=self.habilitafinal)
+        self.textoada = Texto (self.dentrodomuseu, "Essa é a Ada Lovelace. Ela que criou o primeiro algoritmo. Que legal! Depois de toda essa aventura é hora de voltar pra casa")
+        self.ada.vai = self.textoada.vai
+        self.textoada.foi = self.habilitafinal
+        
+        self.talitamuseu.entra(self.dentrodomuseu)
+        self.textotalitamuseu = Texto (self.dentrodomuseu, "Antes de voltar para casa, vou descobrir quem foi que inventou o primeiro algoritmo!")
+        self.talitamuseu.vai = self.textotalitamuseu.vai
+        
+        
+        
+#cena sotao
+        
+        self.ganhamoeda2.direita = self.sotao
+        self.saladeaula2.esquerda = self.ganhamoeda2
+        self.talita4 = Elemento (img = "https://i.imgur.com/QV5fuXJ.png", 
+        tit="talita",
+        style=dict(left=250, top=250, width=120, heigth=1500))
+        self.talita4.entra(self.sotao)
+        self.textotalita4 = Texto (self.sotao, "Vou fazer um trabalho sobre uma área da Ciência em que as mulheres têm se destacado muito atualmente.")
+        self.talita4.vai = self.textotalita4.vai
+        self.textotalita4.foi = self.habilitavolta
+        acha_painel_solar= Elemento (FOCO, x=0, y=0, w=200, h=100, cena=self.sotao, style={"opacity": 0.0}, vai=self.painelsolar.vai)
+
+
+#cenapainelsolar
+
+        self.talita6 = Elemento (img = "https://i.imgur.com/QV5fuXJ.png", 
+        tit="Talita",
+        style=dict(left=250, top=300, width=120, heigth=1500))
+        self.talita6.entra(self.painelsolar)
+        self.textotalita6 = Texto (self.painelsolar, "As mulheres estão dominando o setor de energia renovável. Um grande exemplo disso veio de Nicole Kuepper, uma jovem cientista australiana que recentemente criou células fotovoltáicas - usadas para transformar energia solar em energia elétrica - a partir de produtos parecidos com esmalte e acetona, com baixo custo. Isso deverá ajudar populações que ainda não têm energia elétrica")
+        self.textotalita6.foi = self.habilitavolta
+        self.talita6.vai = self.textotalita6.vai
+       
+
+#cenasotaovolta
+        
+        self.talitaencontrou = Elemento(FOCO, x=500, y=240, w=60, h=100, cena=self.voltasotao, style={"opacity": 0.0}, vai=self.habilitasubmarino)
+        self.textotalitaencontrou = Texto (self.voltasotao, "OMG! Encontrei uma passagem secreta")
+        self.talitaencontrou.vai = self.textotalitaencontrou.vai
+        self.textotalitaencontrou.foi = self.habilitasubmarino
+        
+       
 
     def habilita(self):  # só passa pra sala depois que clicar no colete
         self.introd.direita=self.ganhamoeda
         self.sala = Texto (self.sala, "Esta na hora de ir pra escola").vai()
-      
-        
-        
+ 
     def habilitaquadro(self):  # só passa pra sala depois que clicar no colete
         
         self.submarino.direita=self.ganhadiamante
+        self.ganhadiamante.direita = self.museu
+        
+    def habilitalivro(self):  # só passa pra sala depois que clicar no colete
+        
+        self.saladeaula.direita=self.ganhamoeda2
+   
+    def habilitavolta(self):
+        self.painelsolar.direita=self.ganhamoeda3
+        self.ganhamoeda3.direita = self.voltasotao
+        
+    def habilitasubmarino(self):
+        self.voltasotao.direita = self.submarino
+        
+    def habilitafinal(self):
+        self.dentrodomuseu.direita = self.final
         
     
         
