cb40ca066b17e379eb0e1e5bd9560624508eade9
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-27 18:04:43 -0300

    Saved kristen/main.py @2019-11-27 21:04

--- kristen/main.py
+++ kristen/main.py
@@ -14,10 +14,26 @@ linkdosubmarino="https://i.imgur.com/zuCVdGW.png"
 linkquadro1="https://i.imgur.com/ly6mhuo.jpg"
 linkdoquadro2="https://i.imgur.com/tJlMNQA.jpg"
 linkdosotao="https://i.imgur.com/KrFFLIr.png"
-def Jogo():
-	introd = Cena(img= "https://i.imgur.com/PykSosS.jpg")
+
+class Jogo:
+
+# definicoes de cenas ficam aqui 
+    def __init__(self):
+
+         self.introd = Cena (img = "https://i.imgur.com/PykSosS.jpg")
+         self.entradadaescola = Cena (img = "https://www.google.com/maps/uv?hl=pt-BR&pb=!1s0x99649ea626703b:0x7d4e6aa6d6d9b26c!3m1!7e115!4shttps://lh5.googleusercontent.com/p/AF1QipN_yZCS5Qe_lhaOr_xNN7c4d3WNFF-tkkjHuNYa%3Dw214-h143-k-no!5sescola+municipal+jornalista+e+escritor+daniel+piza+-+Pesquisa+Google&imagekey=!1e10!2sAF1QipN_yZCS5Qe_lhaOr_xNN7c4d3WNFF-tkkjHuNYa&sa=X&ved=2ahUKEwift66hnYvmAhW2I7kGHfOfABkQoiowCnoECAsQBg#")
+         self.quartotalita = cena (img = img="https://i.imgur.com/DEC5m3T.jpg")
+         self.ganhamoeda = Cena (img="https://i.imgur.com/koWP1dw.png")
+         self.ganhamoeda2 = Cena (img="https://i.imgur.com/koWP1dw.png")
+         self.ganhamoeda3 = Cena (img="https://i.imgur.com/koWP1dw.png")
+         self.ganhamoeda.direita=self.sala
+         self.ganhamoeda.esquerda=self.introd
+         self.ganhadiamante = Cena (img="https://i.imgur.com/M9Xx8ab.png")
+         self.sala.direita = self.quartotalita
+
+
 	botaoiniciarjogo = Elemento(img="https://i.imgur.com/Q1nALyV.jpg")
-	quartodatalita = Cena(img="https://i.imgur.com/DEC5m3T.jpg")
+	
 	talita= Elemento (img= linkdatalita,
                      tit="talita",
                      style=dict(left=180, top=50,  Width=30, height=50))
