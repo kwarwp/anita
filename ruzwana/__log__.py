dfab79a5a7b266c2233021d1e32450e80255de27
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-27 09:59:43 -0300

    Saved ruzwana/__log__.py @2019-11-27 12:59

--- ruzwana/__log__.py
+++ ruzwana/__log__.py
@@ -519,3 +519,21 @@ AttributeError: 'bool' object has no attribute 'nome'
                                                                                                                         ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Wed Nov 27 2019 09:59:42.159 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 102
+    Jogo() 
+  module <module> line 26
+    self.escola.direita = self.submarino
+AttributeError: 'Jogo' object has no attribute 'escola'
+'''},
