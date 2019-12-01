58f0721f2c8fe74532e5190d9aa5494a865ca549
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-12-01 16:31:02 -0300

    Saved kristen/__log__.py @2019-12-01 19:31

--- kristen/__log__.py
+++ kristen/__log__.py
@@ -594,3 +594,21 @@ AttributeError: 'Jogo' object has no attribute 'sala'
     self.submarino.esquerda = self.submario1
 AttributeError: 'Jogo' object has no attribute 'submario1'
 '''},
+{'date': 'Sun Dec 01 2019 16:31:01.8 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 171
+    Jogo() 
+  module <module> line 40
+    self.submarino1.esquerda = self.submario1
+AttributeError: 'Jogo' object has no attribute 'submario1'
+'''},
