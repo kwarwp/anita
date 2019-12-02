032c73e9fb72a600dd1fd12779317363ee8e2513
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-12-02 11:08:22 -0300

    Saved stacy/__log__.py @2019-12-02 14:08

--- stacy/__log__.py
+++ stacy/__log__.py
@@ -13,3 +13,10 @@ SyntaxError: invalid syntax
                       ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Mon Dec 02 2019 11:08:24.207 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 6
+  def__init__ (self):
+  ^
+IndentationError: expected an indented block
+'''},
