397dfe25443c531a0fd75fd34bc17ed29dc80b0c
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-12-02 11:58:41 -0300

    Created stacy/main.py @2019-12-02 14:58

---
+++ stacy/main.py
@@ -0,0 +1,18 @@
+# anita.stacy.main.py
+from_spy.vitollino.main import Cena, Elemento, Texto, STYLE
+STYLE ["width"] = 600
+STYLE ["height"] = "200 px"
+
+class Jogo:
+    def __init__(self):
+
+        self.sala = Cena (img = "https://i.imgur.com/gBmTzuA.jpg")
+        sala.vai
+        tit = "Talita"
+        STYLE = dict (left = 200, top = 190, width = 100, height = 1500))
+        self.Talita.entra(self.quarto)
+        
+if __name__ == "__main__":
+    Jogo() 
+        
+        
