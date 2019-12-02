87c10119c6aabce1a6d5386dca82ac35c3f43633
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-12-02 05:12:05 -0300

    Saved kristen/main.py @2019-12-02 08:12

--- kristen/main.py
+++ kristen/main.py
@@ -10,7 +10,6 @@ linklivros="https://i.imgur.com/TwENnNe.gif"
 linkdaescrivaninha="https://i.imgur.com/Rw3pEJu.jpg"
 linkdabola="https://i.imgur.com/YcE5EW6.jpg"
 linkdaescoladatalita= "https://www.google.com/maps/uv?hl=pt-BR&pb=!1s0x99649ea626703b:0x7d4e6aa6d6d9b26c!3m1!7e115!4shttps://lh5.googleusercontent.com/p/AF1QipN_yZCS5Qe_lhaOr_xNN7c4d3WNFF-tkkjHuNYa%3Dw214-h143-k-no!5sescola+municipal+jornalista+e+escritor+daniel+piza+-+Pesquisa+Google&imagekey=!1e10!2sAF1QipN_yZCS5Qe_lhaOr_xNN7c4d3WNFF-tkkjHuNYa&sa=X&ved=2ahUKEwift66hnYvmAhW2I7kGHfOfABkQoiowCnoECAsQBg"
-linkdosubmarino1="https://i.imgur.com/GOH738j.jpg"
 linkdobotao="https://i.imgur.com/Q1nALyV.jpg"
 FOCO = "https://i.imgur.com/6e096Va.png"
 img_moeda = "https://i.imgur.com/tOep9V9.gif"
@@ -107,6 +106,7 @@ class Jogo:
         self.textocolete.foi = self.habilita
         self.talita.vai = self.textotalita.vai
         self.colete.vai = self.textocolete.vai
+        self.livros.vai = self.textolivros.vai
 
 #cena mapa da regiao
          
