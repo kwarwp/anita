4f71e53e9171e38af09aa7c1bc5b83b8666b5ac3
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-12-02 11:13:33 -0300

    Saved soraya/__log__.py @2019-12-02 14:13

--- soraya/__log__.py
+++ soraya/__log__.py
@@ -6,3 +6,10 @@
                                            ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Mon Dec 02 2019 04:13:32.561 GMt-1000 (Horário Padrão do Havaí e Ilhas Aleutas) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 13
+  self.Talita= Elemento (img= linkdatalita
+                                                    ^
+SyntaxError: invalid syntax
+'''},
