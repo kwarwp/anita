3cc896d30cab2edb2e295dac0192e49315ad9d31
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-12-02 11:04:35 -0300

    Saved meredith/main.py @2019-12-02 14:04

--- meredith/main.py
+++ meredith/main.py
@@ -4,8 +4,8 @@ STYLE["width"]= 600
 STYLE["heigh"]="200px"
 linkdacapadojogo="https://i.imgur.com/NwcgtMe.jpg"
 linkdobotao="https://i.imgur.com/gZTKZhv.png"  
-linkdaTalita="https://i.imgur.com/qSWjvPE.png"
-linkquartodaTalita="https://i.imgur.com/DyTUlGG.jpg"
+linkdatalita="https://i.imgur.com/qSWjvPE.png"
+linkquartodatalita="https://i.imgur.com/DyTUlGG.jpg"
 linkcolete = "https://i.imgur.com/SRaVHBw.png"
 linkprotetorsolar = "https://i.imgur.com/1ph5Tne.png"
 FOCO = "https://i.imgur.com/6e096Va.png"
@@ -16,27 +16,27 @@ class Jogo:
     def __init__(self):
     
         self.ganhamoeda = Cena (img="https://i.imgur.com/eqBJynn.png")
-        self.quartodaTalita=Cena(img=linkquartodaTalita)
+        self.quartodatalita=Cena(img=linkquartodatalita)
         
         self.colete = Elemento (img = linkcolete,
         tit = "colete",
         style=dict(left=300, top=140, width=50, heigth=80), vai=self.habilita)
              
-        self.talita = Elemento (img = linkdaTalita,
+        self.talita = Elemento (img = linkdatalita,
         tit="talita",
         style=dict(left=180, top=120,  Width=60, height=50))
 
-        self.talita.entra(self.quartodaTalita)
-        self.colete.entra(self.quartodaTalita)
-        self.textotalita = Texto (self.quartodaTalita, "Olá. Hoje vai ser um dia longo e eu preciso estar preparada para encarar muitos desafios. Hoje sairei de Costa Barros protegida e contarei com uma invenção femina para isso.")
-        self.textocolete = Texto (self.quartodaTalita, "Stephanie Kwolek criou o colete à prova de balas Kevlar, que todos os anos salva a vida de milhares de policiais")
+        self.talita.entra(self.quartodatalita)
+        self.colete.entra(self.quartodatalita)
+        self.textotalita = Texto (self.quartodatalita, "Olá. Hoje vai ser um dia longo e eu preciso estar preparada para encarar muitos desafios. Hoje sairei de Costa Barros protegida e contarei com uma invenção femina para isso.")
+        self.textocolete = Texto (self.quartodatalita, "Stephanie Kwolek criou o colete à prova de balas Kevlar, que todos os anos salva a vida de milhares de policiais")
        
         self.talita.vai = self.textotalita.vai
         self.textocolete.foi = self.habilita
         self.colete.vai = self.textocolete.vai
         
     def habilita(self):  # só passa pra sala depois que clicar no colete
-        self.quartodaTalita.direita=self.ganhamoeda
+        self.quartodatalita.direita=self.ganhamoeda
         
 if __name__ == "__main__":
     Jogo() 
