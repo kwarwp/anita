b749cded2fe7c4e6129b4ddc5c70759310e159c0
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-12-02 14:22:55 -0300

    Saved stacy/__log__.py @2019-12-02 17:22

--- stacy/__log__.py
+++ stacy/__log__.py
@@ -253,3 +253,10 @@ SyntaxError: invalid syntax
                                                                                                         ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Mon Dec 02 2019 14:22:55.787 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 12
+  self.Talita= Elemento (img = "https://i.imgur.com/N5HXcxK.png",
+                                                                                                      ^
+SyntaxError: invalid syntax
+'''},
