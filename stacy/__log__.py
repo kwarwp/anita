bc5cf93cc623ec62150de8519dca305207281024
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-12-02 11:07:32 -0300

    Saved stacy/__log__.py @2019-12-02 14:07

--- stacy/__log__.py
+++ stacy/__log__.py
@@ -6,3 +6,10 @@
                       ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Mon Dec 02 2019 11:07:34.207 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 2
+  from_spy.vitollino.import Cena, Elemento, Texto, STYLE
+                      ^
+SyntaxError: invalid syntax
+'''},
