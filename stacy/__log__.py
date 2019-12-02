008b156b1b9ff215864cbe4a459995783f18f4ca
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-12-02 14:33:56 -0300

    Saved stacy/__log__.py @2019-12-02 17:33

--- stacy/__log__.py
+++ stacy/__log__.py
@@ -338,3 +338,10 @@ SyntaxError: invalid syntax
                                                        ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Mon Dec 02 2019 14:33:56.868 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 14
+  STYLE= dict(left=200, top=190, width=100, height=1500))
+                                                        ^
+SyntaxError: invalid syntax
+'''},
