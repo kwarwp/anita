626a220a46eb656f1e373dea2d841d80fc93d627
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-12-02 11:14:42 -0300

    Saved roxanne/__log__.py @2019-12-02 14:14

--- roxanne/__log__.py
+++ roxanne/__log__.py
@@ -20,3 +20,10 @@ SyntaxError: invalid syntax
                     ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Mon Dec 02 2019 11:14:41.676 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 4
+  STYLE["heigth]= "200px"
+                    ^
+SyntaxError: invalid syntax
+'''},
