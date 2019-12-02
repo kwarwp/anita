637a33fcf2f4afdbee05882ccccc0f8cf63ec7bb
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-12-02 11:02:13 -0300

    Created roxanne/__log__.py @2019-12-02 14:02

---
+++ roxanne/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Mon Dec 02 2019 11:02:10.837 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 4
+  STYLE["heigth]= "200px"
+                    ^
+SyntaxError: invalid syntax
+'''},
