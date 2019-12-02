9d9f9a4bfdb14f4c9b80d92894d71425a379bce1
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-12-01 22:35:48 -0300

    Saved kristen/__log__.py @2019-12-02 01:35

--- kristen/__log__.py
+++ kristen/__log__.py
@@ -956,3 +956,21 @@ AttributeError: 'Jogo' object has no attribute 'sala'
     self.ganhamoeda.esquerda=self.introd
 AttributeError: 'Jogo' object has no attribute 'introd'
 '''},
+{'date': 'Sun Dec 01 2019 22:35:46.375 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 184
+    Jogo() 
+  module <module> line 29
+    self.sala.direita = self.maparegiao
+AttributeError: 'Jogo' object has no attribute 'maparegiao'
+'''},
