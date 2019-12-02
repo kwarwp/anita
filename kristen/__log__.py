03eeb6a6d4a9bca27bfc1f4416846e392626ac02
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-12-02 05:00:38 -0300

    Saved kristen/__log__.py @2019-12-02 08:00

--- kristen/__log__.py
+++ kristen/__log__.py
@@ -1046,3 +1046,21 @@ NameError: name 'FOCO' is not defined
     self.livros = Elemento(FOCO, x=90, y=180, w=80, h=80, cena=self.introd, style={"opacity": 0.0}, vai=self.textolivros.vai)
 AttributeError: 'Jogo' object has no attribute 'textolivros'
 '''},
+{'date': 'Mon Dec 02 2019 05:00:37.337 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 186
+    Jogo() 
+  module <module> line 90
+    self.livros = Elemento (img = linkdolivro,
+NameError: name 'linkdolivro' is not defined
+'''},
