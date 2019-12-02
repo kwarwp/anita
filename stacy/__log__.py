409cfc3822a0dca3d71b6d39642c7799da13d7c7
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-12-02 11:21:19 -0300

    Saved stacy/__log__.py @2019-12-02 14:21

--- stacy/__log__.py
+++ stacy/__log__.py
@@ -104,3 +104,10 @@ IndentationError: expected an indented block
   ^
 IndentationError: expected an indented block
 '''},
+{'date': 'Mon Dec 02 2019 11:21:20.699 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 7
+  self.sala = Cena (img = "https://i.imgur.com/Q57lw3T.jpg")
+  ^
+IndentationError: expected an indented block
+'''},
