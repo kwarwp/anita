4cbc0eeabd1fa2c18a36f8a6b5c1198a9f32a27a
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-12-01 21:52:38 -0300

    Saved kristen/__log__.py @2019-12-02 00:52

--- kristen/__log__.py
+++ kristen/__log__.py
@@ -738,3 +738,10 @@ SyntaxError: EOL while scanning string literal
                                              ^
 SyntaxError: EOL while scanning string literal
 '''},
+{'date': 'Sun Dec 01 2019 21:52:36.995 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 160
+  self.talita6.vai = self.textotalita6.vai"
+                                           ^
+SyntaxError: EOL while scanning string literal
+'''},
