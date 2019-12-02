5656880fbe4c30f26c02429848f45106f7eee483
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-12-02 14:55:19 -0300

    Saved meredith/__log__.py @2019-12-02 17:55

--- meredith/__log__.py
+++ meredith/__log__.py
@@ -744,3 +744,10 @@ SyntaxError: invalid syntax
                                                                                                                                                   ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Mon Dec 02 2019 14:55:18.283 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 21
+  self.colete = Elemento (img = "linkcolete,"
+                                                       ^
+SyntaxError: invalid syntax
+'''},
