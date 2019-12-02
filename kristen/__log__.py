dfe14801edbfd65f6e4334fac11f91c0b6089893
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-12-01 22:45:16 -0300

    Saved kristen/__log__.py @2019-12-02 01:45

--- kristen/__log__.py
+++ kristen/__log__.py
@@ -1028,3 +1028,21 @@ NameError: name 'FOCO' is not defined
     self.diamante = Elemento (FOCO, x=300, y=150, w=200, h=200, cena=self.ganhadiamante, style={"opacity": 0.0}, vai=self.painelsolar.vai)
 NameError: name 'FOCO' is not defined
 '''},
+{'date': 'Sun Dec 01 2019 22:45:15.161 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 188
+    Jogo() 
+  module <module> line 80
+    self.livros = Elemento(FOCO, x=90, y=180, w=80, h=80, cena=self.introd, style={"opacity": 0.0}, vai=self.textolivros.vai)
+AttributeError: 'Jogo' object has no attribute 'textolivros'
+'''},
