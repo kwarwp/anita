27c9c0b343c546f53487ffa846f52b6fe852c33a
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-12-02 12:28:03 -0300

    Saved sarah/__log__.py @2019-12-02 15:28

--- sarah/__log__.py
+++ sarah/__log__.py
@@ -6,3 +6,10 @@
                                                                                                                                                  ^
 SyntaxError: EOL while scanning string literal
 '''},
+{'date': 'Mon Dec 02 2019 12:28:00.63 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 8
+  linkquartodatalita="https://cdna.artstation.com/p/assets/images/images/008/176/322/large/ian-barreto-anymalu-quarto-ianbarreto.jpg?1510953832""
+                                                                                                                                                 ^
+SyntaxError: EOL while scanning string literal
+'''},
