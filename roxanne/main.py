efe559f76f08fc2b137fa533294e94c946b0c35a
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-12-02 14:39:29 -0300

    Saved roxanne/main.py @2019-12-02 17:39

--- roxanne/main.py
+++ roxanne/main.py
@@ -2,6 +2,10 @@
 from _ spy. violino.main import cena,elemento,texto, STYLE
 STYLE["width"]=600
 STYLE["heigth]= "200px"
-linkdatalita="https://i.imgur.com/6rLmVNz.jpg"
+linkdatalita="
 linkdocolete="https://i.imgur.com/PV7WWPJ.jpg"
 linkquartodatalita="https://i.imgur.com/wdKENXo.jpg"
+linkursodatalita=" https://i.imgur.com/DJMDxob.jpg
+linksubmarino=" https://i.imgur.com/fzivk2H.jpg"
+linkescoladatalita= " https://i.imgur.com/9Csr88n.jpg
+link
