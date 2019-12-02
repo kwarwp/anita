43bcd634905f9dd7daad8a705619caf4d77fae8b
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-12-02 14:10:38 -0300

    Saved stacy/__log__.py @2019-12-02 17:10

--- stacy/__log__.py
+++ stacy/__log__.py
@@ -203,3 +203,7 @@ Exception: <SyntaxError: unexpected token: '{'>
 'error': '''
 Exception: <SyntaxError: unexpected token: '{'>
 '''},
+{'date': 'Mon Dec 02 2019 14:10:39.100 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+Exception: <SyntaxError: unexpected token: '{'>
+'''},
