e9286fce130fa8aced227931e8cb6c952bc60eb3
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-12-02 11:57:31 -0300

    Saved natalia/__log__.py @2019-12-02 14:57

--- natalia/__log__.py
+++ natalia/__log__.py
@@ -328,3 +328,21 @@ NameError: name 'quartodatalita' is not defined
     self.talita.vai = textotalita.vai    
 NameError: name 'textotalita' is not defined
 '''},
+{'date': 'Mon Dec 02 2019 04:57:30.898 GMt-1000 (Horário Padrão do Havaí e Ilhas Aleutas) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 46
+    Jogo()    
+  module <module> line 42
+    self.talita.vai = textotalita.vai    
+NameError: name 'textotalita' is not defined
+'''},
