527fe64c9e0f6eb4b7634655c82ec20484e50135
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-12-01 22:41:26 -0300

    Saved kristen/__log__.py @2019-12-02 01:41

--- kristen/__log__.py
+++ kristen/__log__.py
@@ -992,3 +992,21 @@ AttributeError: 'Jogo' object has no attribute 'maparegiao'
     self.diamante = Elemento (FOCO, x=300, y=150, w=200, h=200, cena=self.ganhadiamante, style={"opacity": 0.0}, vai=self.painelsolar.vai)
 NameError: name 'FOCO' is not defined
 '''},
+{'date': 'Sun Dec 01 2019 22:41:24.460 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 185
+    Jogo() 
+  module <module> line 40
+    self.diamante = Elemento (FOCO, x=300, y=150, w=200, h=200, cena=self.ganhadiamante, style={"opacity": 0.0}, vai=self.painelsolar.vai)
+NameError: name 'FOCO' is not defined
+'''},
