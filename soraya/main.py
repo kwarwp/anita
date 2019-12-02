c0645393d44b679b50a77ae5188037725b03c491
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-12-02 11:21:41 -0300

    Saved soraya/main.py @2019-12-02 14:21

--- soraya/main.py
+++ soraya/main.py
@@ -2,28 +2,34 @@
 from _spy.vitollino.main  import Cena, Elemento, Texto,STYLE
 STYLE["width"]= 600
 STYLE["height"]= "200px"
-linkcolete="https://www.camuflagemairsoft.com.br/media/catalog/product/cache/1/image/800x/9df78eab33525d08d6e5fb8d27136e95/4/7/475168341_VERDE.png.png"
-linkdatalita="https://i.pinimg.com/originals/f8/b7/8f/f8b78f6a2e48d6478d606b7ea304b169.png"
-linkquartodatalita="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcREI1tc3ljKAM-C5hZTeauUtZ5sB2_omrRWaqszuHqo9vGDPNPbKg&s"
-def Jogo ():
-	quartodatalita=Cena(img="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcREI1tc3ljKAM-C5hZTeauUtZ5sB2_omrRWaqszuHqo9vGDPNPbKg&s")
-	Talita= Elemento (img= linkdatalita
-				tit="talita",
-				style=dict(left=180, top=50, width=60, heith=50))
+linkcolete="https://i.imgur.com/SRaVHBw.png"
+linkdatalita="https://i.imgur.com/WV9Cd5z.png"
+linkquartodatalita="https://i.imgur.com/2KjQNfD.jpg"
+class Jogo:
+    # definicoes de cenas ficam aqui 
+    def __init__(self):
+        self.quartodatalita=Cena(img="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcREI1tc3ljKAM-C5hZTeauUtZ5sB2_omrRWaqszuHqo9vGDPNPbKg&s")
+        self.ganhamoeda=Cena("https://i.imgur.com/ESvDBWm.png")
+        self.talita= Elemento (img= linkdatalita,
+        tit="talita",
+        style=dict(left=180, top=50, width=60, height=50))
 
-	colete= Elemento(img=linkcolete,
-	tit="colete"
-	style=dict(left=90, top=25, width=30,helih=25))
-    
-		talita.entra(quartodatalita)
-		Conlete.enrta(quartodatalita) 
-	    textotalita=texto(quartodata, " oi esse aqui e o meu quarto,ache oo elemento que a stephanie kudlek criou"
-         textocolete=texto(quartodatalita,"parabens,tenho um presente para vc")
-         talita.vai=textotalita.vai
-         Colete.vai=textocolete.vai
-         quartodatalita.vai()
-     Jogo()
-     Intro()
-link     
+        
+        self.colete = Elemento (img = linkcolete,
+        tit = "colete",
+        style=dict(left=300, top=140, width=50, heigth=80), vai=self.habilita)
+        
+        self.talita.entra(self.quartodatalita)
+        self.colete.entra(self.quartodatalita) 
+        self.textotalita=Texto(self.quartodatalita, " oi esse aqui e o meu quarto,ache oo elemento que a stephanie kudlek criou")
+        self.textocolete=Texto(self.quartodatalita,"parabens,tenho um presente para vc")
+        self.textocolete.foi = self.habilita
+        self.talita.vai = self.textotalita.vai
+        self.colete.vai = self.textocolete.vai
+        self.quartodatalita.vai()
      
-      
+     
+    def habilita(self):  # só passa pra sala depois que clicar no colete
+        self.quartodatalita.direita=self.ganhamoeda
+if __name__ == "__main__":
+    Jogo()   
