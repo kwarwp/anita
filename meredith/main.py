e86805a620b383c4a058e7d42c78fcf9cd131457
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-12-03 06:15:23 -0300

    Saved meredith/main.py @2019-12-03 09:15

--- meredith/main.py
+++ meredith/main.py
@@ -7,7 +7,8 @@ linkdobotao="https://i.imgur.com/gZTKZhv.png"
 linkdatalita="https://i.imgur.com/qSWjvPE.png"
 linkquartodatalita="https://i.imgur.com/wL8HNW8.jpg"
 linkcolete = "https://i.imgur.com/SRaVHBw.png"
-linkprotetorsolar = "https://i.imgur.com/1ph5Tne.png"
+linkdaminie = "https://i.imgur.com/dqYLX4j.jpg"
+linkcaixadesom = "https://i.imgur.com/sCnAvHL.jpg"
 FOCO = "https://i.imgur.com/6e096Va.png"
 
 class Jogo:
@@ -25,7 +26,15 @@ class Jogo:
              
         self.talita = Elemento (img = linkdatalita,
         tit="talita",
-        style=dict(left=180, top=120,  Width=60, height=50))
+        style=dict(left=180, top=120,  Width=60, height=50)
+        
+        self.minie = Elemento (img = linkdaminie,
+        tit="minie",
+        style=dict(left=250, top=80, width=50, height=50)
+        
+        self.caixadesom = Elemento (img = linkdacaixadesom,
+        tit="caixa de som",
+        style=dict(left=400, top=60, width=40, height=40))
 
 
         self.botao = Elemento (img = "https://i.imgur.com/gZTKZhv.png",
@@ -40,10 +49,13 @@ class Jogo:
         self.colete.entra(self.quartodatalita)
         self.textotalita = Texto (self.quartodatalita, "Olá. Hoje vai ser um dia longo e eu preciso estar preparada para encarar muitos desafios. Hoje sairei de Costa Barros protegida e contarei com uma invenção femina para isso.")
         self.textocolete = Texto (self.quartodatalita, "Stephanie Kwolek criou o colete à prova de balas Kevlar, que todos os anos salva a vida de milhares de policiais")
-       
+        self.textominie = Texto (self.quartodatalita, "Olá posso te fazer companhia, mas não sou o que procura agoara!"
+        self.textocaixadesom = Texto (self.quartotalita, "Podemos nos divertir, ao som de uma balada, mas não sou o objeto que procura."
         self.talita.vai = self.textotalita.vai
         self.textocolete.foi = self.habilita
         self.colete.vai = self.textocolete.vai
+        self.textominie.vai = self.textominie.vai
+        self.textocaixadesom.vai = self.textocaixadesom.vai
         self.capa.vai()
         
     def habilita(self):  # só passa pra sala depois que clicar no colete
