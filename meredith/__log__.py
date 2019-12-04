365c976ace144b2c9152c2c23497cc80fe12631d
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-12-04 08:37:45 -0300

    Saved meredith/__log__.py @2019-12-04 11:37

--- meredith/__log__.py
+++ meredith/__log__.py
@@ -779,3 +779,10 @@ SyntaxError: invalid syntax
                                                                                                                                                   ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Wed Dec 04 2019 08:37:44.41 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 27
+  self.talita = Elemento (img = linkdatalita,
+                                                                                                                                                  ^
+SyntaxError: invalid syntax
+'''},
