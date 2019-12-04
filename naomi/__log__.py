ef1a38515e97c7f89c9aad4307208c8fa43238a5
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-12-03 21:10:31 -0300

    Saved naomi/__log__.py @2019-12-04 00:10

--- naomi/__log__.py
+++ naomi/__log__.py
@@ -658,3 +658,21 @@ AttributeError: 'method' object has no attribute 'vai'
     self.textocolete.foi = self.habilita.vai
 AttributeError: 'method' object has no attribute 'vai'
 '''},
+{'date': 'Tue Dec 03 2019 21:10:27.696 GMt-0300 (Hora padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 183
+    Jogo() 
+  module <module> line 99
+    self.textocolete.foi = self.habilita.vai
+AttributeError: 'method' object has no attribute 'vai'
+'''},
