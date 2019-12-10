54b39818e24038cb0e358154756eaeec080b677c
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-12-10 15:52:49 -0300

    Saved naomi/__log__.py @2019-12-10 18:52

--- naomi/__log__.py
+++ naomi/__log__.py
@@ -701,3 +701,21 @@ AttributeError: 'method' object has no attribute 'vai'
                                                     ^
 SyntaxError: EOL while scanning string literal
 '''},
+{'date': 'Tue Dec 10 2019 15:52:48.945 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 183
+    Jogo() 
+  module <module> line 42
+    self.domcasmurro = Elemento(FOCO, x=220, y=320, w=60, h=100, cena=self.saladeaula, style={"opacity": 0.0}, vai=self.textositiodopicapauamarelo.vai)
+AttributeError: 'Jogo' object has no attribute 'textositiodopicapauamarelo'
+'''},
