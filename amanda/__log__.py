43055a262f27f50295cf71fb3a769febca9cd288
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-12-12 13:49:49 -0300

    Saved amanda/__log__.py @2019-12-12 16:49

--- amanda/__log__.py
+++ amanda/__log__.py
@@ -931,3 +931,21 @@ IndentationError: unexpected indent
     self.textocolete.foi = self.habilita
 AttributeError: 'Jogo' object has no attribute 'habilita'
 '''},
+{'date': 'Thu Dec 12 2019 13:49:39.612 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 60
+    Jogo() 
+  module <module> line 46
+    self.coletedatalita.vai = self.textocolete.vai
+AttributeError: 'Jogo' object has no attribute 'textocolete'
+'''},
