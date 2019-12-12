8c46f23b95795f07588a40a9b3bece57fa298ad1
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-12-12 13:56:04 -0300

    Saved naomi/__log__.py @2019-12-12 16:56

--- naomi/__log__.py
+++ naomi/__log__.py
@@ -791,3 +791,21 @@ AttributeError: 'Jogo' object has no attribute 'textocapitaesdeareia'
     self.iracema = Elemento(FOCO, x=370, y=330, w=70, h=100, cena=self.saladeaula, style={"opacity": 0.0}, vai=self.textomemoriaspostumas.vai)
 AttributeError: 'Jogo' object has no attribute 'textomemoriaspostumas'
 '''},
+{'date': 'Thu Dec 12 2019 14:56:02.449 GMt-0200 (Horário de Verão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 198
+    Jogo() 
+  module <module> line 66
+    self.oturistaaprendiz = Elemento(FOCO, x=110, y=120, w=60, h=100, cena=self.saladeaula, style={"opacity": 0.0}, vai=self.textooguarani.vai)
+AttributeError: 'Jogo' object has no attribute 'textooguarani'
+'''},
