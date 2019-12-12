da2ddbc00d6aec9935698e805f982ba20bf9cb70
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-12-12 12:11:06 -0300

    Saved amanda/__log__.py @2019-12-12 15:11

--- amanda/__log__.py
+++ amanda/__log__.py
@@ -625,3 +625,10 @@ IndentationError: unexpected indent
                                           ^
 SyntaxError: EOL while scanning string literal
 '''},
+{'date': 'Thu Dec 12 2019 12:10:57.840 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 9
+  linkcapadojogo=" module <string> line 26
+                                          ^
+SyntaxError: EOL while scanning string literal
+'''},
