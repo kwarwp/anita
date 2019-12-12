70c9ba48d15b86a9ebd2183806adceaf02ffd46d
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-12-12 13:14:14 -0300

    Saved amanda/__log__.py @2019-12-12 16:14

--- amanda/__log__.py
+++ amanda/__log__.py
@@ -862,3 +862,7 @@ NameError: <ReferenceError: no_set_attr is not defined>
 'error': '''
 NameError: <ReferenceError: no_set_attr is not defined>
 '''},
+{'date': 'Thu Dec 12 2019 13:14:05.585 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+NameError: <ReferenceError: no_set_attr is not defined>
+'''},
