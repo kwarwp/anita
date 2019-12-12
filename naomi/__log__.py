7e0fdc7894b0009e72e4f0483c30f452a30d5f52
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-12-12 13:57:22 -0300

    Saved naomi/__log__.py @2019-12-12 16:57

--- naomi/__log__.py
+++ naomi/__log__.py
@@ -809,3 +809,21 @@ AttributeError: 'Jogo' object has no attribute 'textomemoriaspostumas'
     self.oturistaaprendiz = Elemento(FOCO, x=110, y=120, w=60, h=100, cena=self.saladeaula, style={"opacity": 0.0}, vai=self.textooguarani.vai)
 AttributeError: 'Jogo' object has no attribute 'textooguarani'
 '''},
+{'date': 'Thu Dec 12 2019 14:57:21.417 GMt-0200 (Horário de Verão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 202
+    Jogo() 
+  module <module> line 118
+    self.textocolete.foi = self.habilita.vai
+AttributeError: 'method' object has no attribute 'vai'
+'''},
