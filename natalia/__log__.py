bf4fd227838b2bb07c88648d034ea64528d77a4c
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-12-12 14:19:05 -0300

    Saved natalia/__log__.py @2019-12-12 17:19

--- natalia/__log__.py
+++ natalia/__log__.py
@@ -392,3 +392,10 @@ SyntaxError: EOL while scanning string literal
                           ^
 SyntaxError: EOL while scanning string literal
 '''},
+{'date': 'Thu Dec 12 2019 15:19:04.764 GMt-0200 (Horário de Verão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 4
+  STYLE["heigth"] = "200px
+                          ^
+SyntaxError: EOL while scanning string literal
+'''},
