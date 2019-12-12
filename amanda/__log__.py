331d682b3d75fc250d52e576a9e8ba7ef86df832
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-12-12 13:58:55 -0300

    Saved amanda/__log__.py @2019-12-12 16:58

--- amanda/__log__.py
+++ amanda/__log__.py
@@ -967,3 +967,21 @@ AttributeError: 'Jogo' object has no attribute 'textocolete'
     self.coletedatalita = Elemento(FOCO, x=220, y=320, w=60, h=100, cena=self.quartodatalita, style={"opacity": 0.0}, vai=self.textocoletedatalita.vai)
 NameError: name 'FOCO' is not defined
 '''},
+{'date': 'Thu Dec 12 2019 13:58:45.736 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 69
+    Jogo() 
+  module <module> line 52
+    self.textocolete = Texto (self.introd, "Stephanie Kwolek criou o colete à prova de balas Kevlar, que todos os anos salva a vida de milhares de policiais")
+AttributeError: 'Jogo' object has no attribute 'introd'
+'''},
