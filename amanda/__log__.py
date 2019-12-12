18ca72dcad60252ccca148123c4de3bc64c0e8d2
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-12-12 14:20:59 -0300

    Saved amanda/__log__.py @2019-12-12 17:20

--- amanda/__log__.py
+++ amanda/__log__.py
@@ -1102,3 +1102,21 @@ NameError: <ReferenceError: no_set_attr is not defined>
   ^
 IndentationError: unexpected indent
 '''},
+{'date': 'Thu Dec 12 2019 14:20:49.405 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 55
+    Jogo() 
+  module <module> line 25
+    self.sala = Texto (self.sala, "Esta na hora de ir pra escola") 
+AttributeError: 'Jogo' object has no attribute 'sala'
+'''},
