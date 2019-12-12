ac2d1a3dc891e2d7252022575e89d753923c234e
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-12-12 14:03:18 -0300

    Saved amanda/__log__.py @2019-12-12 17:03

--- amanda/__log__.py
+++ amanda/__log__.py
@@ -1021,3 +1021,21 @@ AttributeError: 'Jogo' object has no attribute 'introd'
     self.textocolete = Texto (self.introd, "Stephanie Kwolek criou o colete à prova de balas Kevlar, que todos os anos salva a vida de milhares de policiais")
 AttributeError: 'Jogo' object has no attribute 'introd'
 '''},
+{'date': 'Thu Dec 12 2019 14:03:09.463 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 69
+    Jogo() 
+  module <module> line 52
+    self.textocolete = Texto (self.introd, "Stephanie Kwolek criou o colete à prova de balas Kevlar, que todos os anos salva a vida de milhares de policiais")
+AttributeError: 'Jogo' object has no attribute 'introd'
+'''},
