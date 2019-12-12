34b8682851fa5193decefbe00a8995b66cd9f056
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-12-12 12:37:40 -0300

    Saved lisa/__log__.py @2019-12-12 15:37

--- lisa/__log__.py
+++ lisa/__log__.py
@@ -1016,3 +1016,21 @@ AttributeError: 'Jogo' object has no attribute 'quartodatalita'
     self.capa.direita=self.quartodatalita
 AttributeError: 'Jogo' object has no attribute 'quartodatalita'
 '''},
+{'date': 'Thu Dec 12 2019 12:37:31.460 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 73
+    Jogo() 
+  module <module> line 56
+    self.capa.direita=self.quartodatalita
+AttributeError: 'Jogo' object has no attribute 'quartodatalita'
+'''},
