3a627fd415c192c525fc0908934b951898873389
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-12-12 12:54:53 -0300

    Saved amanda/__log__.py @2019-12-12 15:54

--- amanda/__log__.py
+++ amanda/__log__.py
@@ -775,3 +775,21 @@ AttributeError: 'Jogo' object has no attribute 'ganhamoeda'
     self.quartodatalita.direita=self.ganhamoeda
 AttributeError: 'Jogo' object has no attribute 'ganhamoeda'
 '''},
+{'date': 'Thu Dec 12 2019 12:54:44.450 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 40
+    Jogo() 
+  module <module> line 28
+    self.quartodatalita.direita=self.ganhamoeda
+AttributeError: 'Jogo' object has no attribute 'ganhamoeda'
+'''},
