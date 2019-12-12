95d0a11a6fb8c5055d01209629dd702efc5850a8
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-12-12 13:07:45 -0300

    Saved amanda/__log__.py @2019-12-12 16:07

--- amanda/__log__.py
+++ amanda/__log__.py
@@ -836,3 +836,21 @@ SyntaxError: invalid syntax
     self.ganhamoeda.direita=self.sala
 AttributeError: 'Jogo' object has no attribute 'sala'
 '''},
+{'date': 'Thu Dec 12 2019 13:07:36.239 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 48
+    Jogo() 
+  module <module> line 36
+    self.sala.direita=self.mapadaregiao
+AttributeError: 'Jogo' object has no attribute 'mapadaregiao'
+'''},
