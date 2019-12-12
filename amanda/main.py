b1ddb19e8371d21860f53aa74050f65b09863182
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-12-12 13:02:27 -0300

    Saved amanda/main.py @2019-12-12 16:02

--- amanda/main.py
+++ amanda/main.py
@@ -29,8 +29,10 @@ class Jogo:
     
     self.quartodatalita.direita=self.ganhamoeda
     self.ganhamoeda.esquerda=self.quartodatalita
+    self.ganhamoeda.direita=self.sala
     
-   
+    self.sala=Cena(img"https://png.pngtree.com/thumb_back/fw800/background/20190222/ourmid/pngtree-neat-living-room-cartoon-background-roomsofamuralplantarrangementcartoonbackgroundtidy-image_63917.jpg")
+    
     self.capa.vai()
     
     self.submarino=Cena(img= "https://i.imgur.com/GOH738j.jpg")
