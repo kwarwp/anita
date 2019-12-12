9a95ea54e19547788a73ebb187e852e250fb7ace
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-12-12 12:10:06 -0300

    Saved lisa/main.py @2019-12-12 15:10

--- lisa/main.py
+++ lisa/main.py
@@ -11,15 +11,21 @@ linksubmarino="https://i.imgur.com/7bsWL02.jpg"
 linkquadro1= "https://i.imgur.com/x0JjYNb.jpg"
 linkquadro2= "https://i.imgur.com/xSE0HV4.jpg"
 linksotao="https://i.imgur.com/7bsWL02.jpg"
+FOCO = "https://i.imgur.com/6e096Va.png"
+
+
 class Jogo:
     def __init__(self):
     
-        self.Intro = Cena (img= "https://image.freepik.com/vetores-gratis/modelo-de-plano-de-fundo-interior-quarto-dos-desenhos-animados-aconchegante-casa-moderna-sala-na-luz-da-manha_33099-171.jpg")
+        self.introd = Cena (img= "https://image.freepik.com/vetores-gratis/modelo-de-plano-de-fundo-interior-quarto-dos-desenhos-animados-aconchegante-casa-moderna-sala-na-luz-da-manha_33099-171.jpg")
         self.sala = Cena (img= "https://i.imgur.com/hBgCTYo.jpg")
         self.maparegiao = Cena(img="https://i.imgur.com/MGJSDE3.png")
         self.ganhamoeda = Cena (img="https://i.imgur.com/koWP1dw.png")
         self.ganhamoeda2 = Cena (img="https://i.imgur.com/koWP1dw.png")
         self.ganhamoeda3 = Cena (img="https://i.imgur.com/koWP1dw.png")
+        self.ganhadiamante= Cena(img="https://images.vexels.com/media/users/3/151691/isolated/preview/2dcb5661394335eaa59671bad4e24eb4---cone-isolado-de-diamante-by-vexels.png")
+        #self.introd.vai() 
+        
         self.ganhamoeda.direita=self.sala
         self.ganhamoeda.esquerda=self.introd
         self.sala.direita = self.maparegiao
@@ -32,31 +38,33 @@ class Jogo:
         self.rampaescola.esquerda = self.patioescola
         self.ganhadiamante.esquerda = self.submarino
         self.painelsolar = Cena (img= "https://i.imgur.com/xfYAwKB.jpg")
+        
         self.diamante = Elemento (FOCO, x=300, y=150, w=200, h=200, cena=self.ganhadiamante, style={"opacity": 0.0}, vai=self.painelsolar.vai)
         self.sotao = Cena (img="https://i.imgur.com/E9op9Yf.jpg")
-        self.voltasotao = Cena ("https://i.imgur.com/7bsWL02.jpg")
+        self.voltasotao = Cena (img="https://i.imgur.com/7bsWL02.jpg")
         
         
         
     
-        talita = Elemento (img =linkdatalita, tit="talita", style=dict (left=300,top=50,width=60,height=50))
-        colete = Elemento (img= linkdocolete, tit="colete", style=dict (left=50,top=250,width=50,height=200))
-        barbie = Elemento (img= linkdabarbie, tit="barbie",  style=dict (left=50,top=250,width=50,height=200))
+        self.talita = Elemento (img =linkdatalita, tit="talita", style=dict (left=300,top=50,width=60,height=50))
+        self.colete = Elemento (img= linkdocolete, tit="colete", style=dict (left=50,top=250,width=50,height=200))
+        self.barbie = Elemento (img= linkdabarbie, tit="barbie",  style=dict (left=50,top=250,width=50,height=200))
 
-                        
+        #Cena quarto da talita                
                        
-                    
-        talita.entra(quartodatalita)
-        colete.entra(quartodatalita)
-        textotalita=Texto(quartodatalita,"Para eu sair do quarto preciso que vc ache para mim o objeto que stephanie kudlek criou"/
-        textocolete=Texto (quartodatalita, "parabéns,vc encontou o colete!tenho um presente pra vc")
-        talita.vai=textotalita.vai
-        colete.vai=textocolete.vai
-        quartodatalita.vai()
+        self.quartodatalita=Cena(img="https://i.imgur.com/5kB6A8i.jpg")           
+        self.talita.entra(self.quartodatalita)
+        self.colete.entra(self.quartodatalita)
+        self.textotalita=Texto(self.quartodatalita,"Para eu sair do quarto preciso que vc ache para mim o objeto que stephanie kudlek criou")
+        self.textocolete=Texto (self.quartodatalita, "parabéns,vc encontou o colete!tenho um presente pra vc")
+        self.talita.vai=self.textotalita.vai
+        self.colete.vai=self.textocolete.vai
+        self.quartodatalita.vai()
     #Jogo()
-       Sumbmarino=cena(img= "https://i.imgur.com/7bsWL02.jpg
-       quadro1= Elemento(img= "linkquadro1", tit="quadro1", style= dict(left=120, top=40, heigth=60))
-       quadro2=Elemento(img= "linkquadro2",
+       #self.sumbmarino=Cena(img= "https://i.imgur.com/7bsWL02.jpg")
+        self.Submarino = Cena (img="https://i.imgur.com/7bsWL02.jpg")
+        self.quadro1= Elemento(img= "linkquadro1", tit="quadro1", style= dict(left=120, top=40, heigth=60))
+       #self.quadro2=Elemento(img= "linkquadro2",
                     
-                   
- Jogo() 
+if __name__ == "__main__":
+    Jogo() 
