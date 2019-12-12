80a2f4c45a299b7d9440b2df316e0c5907dd1639
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-12-12 12:21:27 -0300

    Saved amanda/__log__.py @2019-12-12 15:21

--- amanda/__log__.py
+++ amanda/__log__.py
@@ -653,3 +653,10 @@ SyntaxError: EOL while scanning string literal
                                           ^
 SyntaxError: EOL while scanning string literal
 '''},
+{'date': 'Thu Dec 12 2019 12:21:18.598 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 9
+  linkcapadojogo=" module <string> line 26
+                                          ^
+SyntaxError: EOL while scanning string literal
+'''},
