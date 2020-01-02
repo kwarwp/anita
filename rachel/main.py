1b6fdbc690d842fe704ebac63e30865699e2b09b
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-01-02 16:25:58 -0300

    Saved rachel/main.py @2020-01-02 19:25

--- rachel/main.py
+++ rachel/main.py
@@ -171,12 +171,15 @@ class Jogo:
         self.talitaencontrou = Elemento(FOCO, x=500, y=240, w=60, h=100, cena=self.voltasotao, style={"opacity": 0.0}, vai=self.textotalitaencontrou.vai)
 #cenahospital
         self.gertrudes.entra(self.hospital)
-        self.talita6.entra(self.hospital)
-       
-       
-       
-       
-       
+        self.talita7 = Elemento (img = "https://i.imgur.com/QV5fuXJ.png", 
+        tit="Talita",
+        style=dict(left=250, top=300, width=120, heigth=1500))
+        self.talita7.entra(self.hospital)
+        self.textotalita7 = Texto (self.hospital, "As mulheres estão dominando o setor de energia renovável. Um grande exemplo disso veio de Nicole Kuepper, uma jovem cientista australiana que recentemente criou células fotovoltáicas - usadas para transformar energia solar em energia elétrica - a partir de produtos parecidos com esmalte e acetona, com baixo custo. Isso deverá ajudar populações que ainda não têm energia elétrica")
+        #self.textotalita7.foi = self.habilitafica
+        self.talita7.vai = self.textotalita7.vai
+        
+
     def habilita(self):  # só passa pra sala depois que clicar no colete
         self.introd.direita=self.ganhamoeda
         self.sala = Texto (self.sala, "Esta na hora de ir pra escola").vai()
@@ -192,6 +195,8 @@ class Jogo:
     def habilitavolta(self):
         self.painelsolar.direita=self.ganhamoeda3
         self.ganhamoeda3.direita = self.submarino
+        
+   
         
 if __name__ == "__main__":
     Jogo() 
