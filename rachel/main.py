ab87e9da4b9af295ae3940849927a2dd15a8f6d3
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-01-02 18:12:19 -0300

    Saved rachel/main.py @2020-01-02 21:12

--- rachel/main.py
+++ rachel/main.py
@@ -170,24 +170,34 @@ class Jogo:
         self.talitaencontrou = Elemento(FOCO, x=500, y=240, w=60, h=100, cena=self.voltasotao, style={"opacity": 0.0}, vai=self.textotalitaencontrou.vai)
 #cenahospital
        
+       
+        self.talita7=Elemento(img= "https://i.imgur.com/N5HXcxK.png",
+        tit="Talita",
+        style=dict(left=250, top=300, width=120, heigth=1500))
+        self.talita7.entra(self.hospital)
+        self.textotalita = Texto(self.hospital, "  jjj")
+        self.talita7.vai=self.textotalita.vai
+        
+        
+        
         self.mar= Cena (img= "https://s3-sa-east-1.amazonaws.com/uploads-astrocentro/blog/wp-content/uploads/2017/04/06121758/sonhar-com-mar.jpg")
         self.gertrude = Elemento(img= "https://i.imgur.com/RCxLvpr.jpg",
         tit ="Gertrude Elion", 
-        style=dict(left=50, top=70, width=100, heigth=80, vai=self.habilitagertrude))
+        style=dict(left=50, top=50, width=80, heigth=80, vai=self.habilitagertrude))
         self.textogertrude = Texto (self.hospital, "Essa é a Gertrude Elion, bioquímica e desenvolvedora do tratamento de leucemia") 
         self.textogertrude.foi = self.habilitagertrude
         self.gertrude.vai = self.textogertrude.vai
         
         self.edward = Elemento(img= "https://i.imgur.com/WYqmfnH.jpg",
         tit ="Edward Jenner", 
-        style=dict(left=300, top=70, width=100, heigth=80))
+        style=dict(left=350, top=50, width=80, heigth=50))
         self.textoedward = Texto (self.hospital, "Edward Jenner- inventou a vacina da varíola. A primeira imunização deste tipo na história do ocidente.")
         self.edward.vai=self.textoedward.vai
         
         
         self.michelle= Elemento (img= "https://i.imgur.com/FHR17Yd.jpg",
         tit = "Michelle Ménard",
-        style=dict(left=200, top=70, width=150, heigth=80))
+        style=dict(left=200, top=50, width=90, heigth=80))
         self.textomichelle= Texto(self.hospital, " Essa é a Michelle Ménard, criadora do esmalte para unhas no século xx.")
         self.michelle.vai=self.textomichelle.vai
         
