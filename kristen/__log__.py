7293d86b160c799e61302dc4dbc802cabd956a0b
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-01-02 18:15:19 -0300

    Saved kristen/__log__.py @2020-01-02 21:15

--- kristen/__log__.py
+++ kristen/__log__.py
@@ -1580,3 +1580,21 @@ AttributeError: 'Jogo' object has no attribute 'saladeaula2'
     self.saladeaula2.esquerda = self.ganhamoeda2
 AttributeError: 'Jogo' object has no attribute 'saladeaula2'
 '''},
+{'date': 'Thu Jan 02 2020 18:15:18.454 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 192
+    Jogo() 
+  module <module> line 150
+    self.saladeaula2.esquerda = self.ganhamoeda2
+AttributeError: 'Jogo' object has no attribute 'saladeaula2'
+'''},
