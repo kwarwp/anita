a5530a534cb31ed91af81fd7b1c29246be4267a0
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-01-02 16:08:50 -0300

    Saved rachel/__log__.py @2020-01-02 19:08

--- rachel/__log__.py
+++ rachel/__log__.py
@@ -977,3 +977,21 @@ AttributeError: 'Jogo' object has no attribute 'sotao'
     self.ganhadiamante.direita = self.hospital
 AttributeError: 'Jogo' object has no attribute 'hospital'
 '''},
+{'date': 'Thu Jan 02 2020 16:08:48.927 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 189
+    Jogo() 
+  module <module> line 35
+    self.diamante = Elemento (FOCO, x=300, y=150, w=200, h=200, cena=self.ganhadiamante, style={"opacity": 0.0}, vai=self.hospital.vai)
+AttributeError: 'Jogo' object has no attribute 'hospital'
+'''},
