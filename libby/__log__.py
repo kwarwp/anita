d63d6dcbf7a96a9b81804e78fbd8936c1b2de856
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-01-02 22:33:34 -0300

    Created libby/__log__.py @2020-01-03 01:33

---
+++ libby/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Thu Jan 02 2020 22:33:31.633 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 152
+  self.oi=Elemento(img ="
+                         ^
+SyntaxError: EOL while scanning string literal
+'''},
