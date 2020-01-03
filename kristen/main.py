4eb5ae0442685a6492689c0e33e9b96453559bd5
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-01-02 22:02:31 -0300

    Saved kristen/main.py @2020-01-03 01:02

--- kristen/main.py
+++ kristen/main.py
@@ -147,27 +147,7 @@ class Jogo:
 #cena sotao
         self.ganhamoeda2.direita = self.sotao
         self.saladeaula2.esquerda = self.ganhamoeda2
-        self.talita10 = Elemento (img = "https://i.imgur.com/QV5fuXJ.png", 
-        tit="Talita",
-        style=dict(left=250, top=250, width=120, heigth=1500))
-        self.talita10.entra(self.sotao)
-        #self.textotalita4 = Texto (self.sotao, "Vou fazer um trabalho sobre uma área da Ciência em que as mulheres têm se destacado muito atualmente.")
-        #self.talita4.vai = self.textotalita4.vai
-        #self.textotalita4.foi = self.habilitavolta
-        #acha_painel_solar= Elemento (FOCO, x=0, y=0, w=200, h=100, cena=self.sotao, style={"opacity": 0.0}, vai=self.painelsolar.vai)
-       #'''self.ganhamoeda2.direita = self.sotao
-        #self.saladeaula2.esquerda = self.ganhamoeda2
-        '''self.talita4 = Elemento (img = "https://i.imgur.com/JiJFDay.png", 
-        tit="talita",
-        style=dict(left=250, top=250, width=120, heigth=1500))
-        self.talita4.entra(self.sotao)
-        self.textotalita4= Texto(self.sotao, "Vou fazer um trabalho sobre uma área da Ciência em que as mulheres têm se destacado muito atualmente.")
-        self.talita4.vai=self.textotalita4.vai
-        #self.textotalita4 = Texto (self.sotao, "Vou fazer um trabalho sobre uma área da Ciência em que as mulheres têm se destacado muito atualmente.")
-        #self.talita4.vai = self.textotalita4.vai
-        #self.textotalita4.foi = self.habilitavolta
-       # acha_painel_solar= Elemento (FOCO, x=0, y=0, w=200, h=100, cena=self.sotao, style={"opacity": 0.0}, vai=self.painelsolar.vai)'''
-
+       
 #cenapainelsolar
 
         self.talita5 = Elemento (img = "https://i.imgur.com/2LZFFjU.png", 
