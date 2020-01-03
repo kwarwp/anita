d0f31aa146b928c735c9f94473ff93cd0dedb387
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-01-02 21:45:39 -0300

    Saved kristen/__log__.py @2020-01-03 00:45

--- kristen/__log__.py
+++ kristen/__log__.py
@@ -1695,3 +1695,21 @@ AttributeError: 'Jogo' object has no attribute 'saladeaula2'
                                                                                                                                ^
 SyntaxError: invalid syntax : triple string end not found
 '''},
+{'date': 'Thu Jan 02 2020 21:45:37.292 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 192
+    Jogo() 
+  module <module> line 156
+    self.talita4.vai = self.textotalita4.vai
+AttributeError: 'Jogo' object has no attribute 'talita4'
+'''},
