60bfcb340f9527e63898edc93fe5f5c54074dfc8
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2020-01-05 14:25:48 -0300

    Saved kristen/__log__.py @2020-01-05 17:25

--- kristen/__log__.py
+++ kristen/__log__.py
@@ -2451,3 +2451,21 @@ IndentationError: unexpected indent
     self.saladeaula2.esquerda = self.ganhamoeda2
 AttributeError: 'Jogo' object has no attribute 'saladeaula2'
 '''},
+{'date': 'Sun Jan 05 2020 14:25:47.284 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 17
+    class Jogo:
+  module <module> line 152
+    self.ganhamoeda2.direita = self.sotao
+AttributeError: 'GUI' object has no attribute 'ganhamoeda2'
+'''},
